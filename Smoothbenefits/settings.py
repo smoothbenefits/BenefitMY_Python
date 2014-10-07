95be08b4d48ab61c223dcbe80e86e11a75c880b6
Lin Lin <Linlin.cqu@gmail.com>
2014-10-06 22:04:27 -0400

    Add postgres DB connection config

--- Smoothbenefits/settings.py
+++ Smoothbenefits/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'Smoothbenefits.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Benefits_DB',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
