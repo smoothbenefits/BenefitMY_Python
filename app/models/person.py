0f1b58e57eb318326fa1b938a81dfd3c995fe65b
Lin Lin <Linlin.cqu@gmail.com>
2014-10-09 22:06:29 -0400

    add more fields to person model

--- app/models/person.py
+++ app/models/person.py
@@ -4,8 +4,14 @@ from phone import Phone
 
 
 class Person(models.Model):
-    person_type = models.CharField(max_length=30)
+    obj_id = models.IntegerField()
+    obj_type = models.TextField()    
+    person_type = models.CharField(max_length=30) 
+    full_name = models.TextField()
+    email = models.TextField()
     relationship = models.CharField(max_length=30, blank=True)
+    ssn = models.TextField()
+    birth_date = models.DateField()
     addresses = models.ForeignKey(Address)
     phones = models.ForeignKey(Phone)
 
