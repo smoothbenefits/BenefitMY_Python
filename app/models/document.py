e17706249b4089db4999b58116c60996531fa924
Lin Lin <Linlin.cqu@gmail.com>
2014-10-15 23:18:07 -0400

    update the document model

--- app/models/document.py
+++ app/models/document.py
@@ -1,11 +1,15 @@
 from django.db import models
+from company import Company
+from user import User
+from template import Template
+from document_type import DocumentType
 
 
 class Document(models.Model):
-    company_id = models.IntegerField()    
-    user_id = models.IntegerField()    
-    template_id = models.IntegerField()    
-    document_type_id = models.IntegerField()    
-    name = models.TextField()    
-    content = models.TextField()  
-    edited = models.BooleanField(default=False)  
+    company = models.ForeignKey(Company)
+    user = models.ForeignKey(User)
+    template = models.ForeignKey(Template)
+    document_type = models.ForeignKey(DocumentType)
+    name = models.TextField()
+    content = models.TextField()
+    edited = models.BooleanField(default=False)
--- app/models/document_type.py
+++ app/models/document_type.py
@@ -2,5 +2,4 @@ from django.db import models
 
 
 class DocumentType(models.Model):
-    name = models.TextField()    
-  
+    name = models.TextField()
