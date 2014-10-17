a10574c791074c5475a01b0aaa32bc584ea44247
Lin Lin <Linlin.cqu@gmail.com>
2014-10-16 22:24:08 -0400

    update benefit type

--- app/models/benefit_type.py
+++ app/models/benefit_type.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+TYPES = (('Medical', 'Medical'),
+         ('Dental', 'Dental'),
+         ('Vision', 'Vision'))
+
 
 class BenefitType(models.Model):
-    name = models.TextField()
+    name = models.TextField(choices=TYPES)
     display_priority = models.IntegerField()
