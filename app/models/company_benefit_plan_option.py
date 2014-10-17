747ef5ea909144a8df5339fe65b5fbdfddbbb769
Lin Lin <Linlin.cqu@gmail.com>
2014-10-16 22:38:31 -0400

    update company benefit type choices

--- app/models/company_benefit_plan_option.py
+++ app/models/company_benefit_plan_option.py
@@ -2,10 +2,20 @@ from django.db import models
 from company import Company
 from benefit_plan import BenefitPlan
 
+S = ["individual",
+     "individual_plus_spouse",
+     "individual_plus_child",
+     "individual_plus_one",
+     "individual_plus_children",
+     "family"]
+
+TYPES = ([(item, item) for item in S])
+
+
 class CompanyBenefitPlanOption(models.Model):
     total_cost_per_period = models.DecimalField()
     employ_cost_per_period = models.DecimalField()
-    benefit_option_type = models.TextField()
+    benefit_option_type = models.TextField(choices=TYPES)
 
     company = models.ForeignKey(Company)
     benefit_plan = models.ForeignKey(BenefitPlan)
