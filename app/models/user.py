9efa24061852a27db0729efe15c49bd5d2d4a862
Lin Lin <Linlin.cqu@gmail.com>
2014-10-17 22:37:17 -0400

    change the user model to django auth models

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,9 @@
 from django.db import models
 from company import Company
 from company_user import CompanyUser
+from django.contrib.auth.models import User
 
 
 class User(models.Model):
-    full_name = models.TextField()
-    email = models.EmailField(max_length=255)
-    encrypted_password = models.TextField(null=True)
-    tz = models.TextField(null=True)
+    user = models.OneToOneField(User)
     company = models.ManyToManyField(Company, through='CompanyUser')
