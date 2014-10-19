4fc8e880cb2cdc5f669eca031e864161da0c695a
Lin Lin <linlin.cqu@gmail.com>
2014-10-19 12:21:24 -0400

    update phone field type

--- app/models/phone.py
+++ app/models/phone.py
@@ -3,6 +3,9 @@ from person import Person
 
 
 class Phone(models.Model):
-    phone_type = models.TextField()
-    number = models.TextField()
-    person = models.ForeignKey(Person, related_name='phones')
+    phone_type = models.CharField(max_length=10)
+    number = models.CharField(max_length=32)
+    person = models.ForeignKey(Person,
+                               related_name='phones',
+                               blank=True,
+                               null=True)
