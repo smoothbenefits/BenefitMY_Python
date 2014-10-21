46de5553336ed8782e9b845201661140c4422c20
Lin Lin <linlin.cqu@gmail.com>
2014-10-20 23:04:44 -0400

    add document raw data

--- app/models/document.py
+++ app/models/document.py
@@ -8,8 +8,8 @@ from document_type import DocumentType
 class Document(models.Model):
     company = models.ForeignKey(Company)
     user = models.ForeignKey(User)
-    template = models.ForeignKey(Template)
-    document_type = models.ForeignKey(DocumentType)
+    template = models.ForeignKey(Template, null=True, blank=True)
+    document_type = models.ForeignKey(DocumentType, null=True, blank=True)
     name = models.CharField(max_length=255)
     content = models.TextField()
     edited = models.BooleanField(default=False)
