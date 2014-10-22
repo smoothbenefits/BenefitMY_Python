ffcf842f885ed4cf3c6e2043d965fbef5250dd64
Lin Lin <linlin.cqu@gmail.com>
2014-10-22 18:11:31 -0400

    add the document field raw data

--- app/models/document_field.py
+++ app/models/document_field.py
@@ -3,6 +3,6 @@ from document import Document
 
 
 class DocumentField(models.Model):
-    document_id = models.ForeignKey(Document, related_name="document_field")
+    document = models.ForeignKey(Document, related_name="document_field")
     name = models.CharField(max_length=255)
     value = models.TextField()
