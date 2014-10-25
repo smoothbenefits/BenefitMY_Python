3ac764baa802b77b8b0ca9a6c87e90b866ae5fdf
Zimeng Chen <eagleofpc@gmail.com>
2014-10-25 09:07:47 -0400

    try to integrate with the home page attempt #1

--- Smoothbenefits/settings.py
+++ Smoothbenefits/settings.py
@@ -89,4 +89,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    # location of your application, should not be public web accessible 
+    os.path.join(os.path.join(BASE_DIR, 'app'), 'media'),
+)
 
--- Smoothbenefits/urls.py
+++ Smoothbenefits/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from app.views import register_view, login_view
+from app.views import register_view, login_view, home_view
 
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Smoothbenefits.views.home', name='home'),
+    url(r'^$', home_view.index, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
