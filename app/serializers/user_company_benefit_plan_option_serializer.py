55c000af43a36a5d8089dc89c382fce1bbb24ae1
Lin Lin <linlin.cqu@gmail.com>
2014-10-25 09:39:31 -0400

    user enrolled benefits include family now

--- app/serializers/user_company_benefit_plan_option_serializer.py
+++ app/serializers/user_company_benefit_plan_option_serializer.py
@@ -4,10 +4,12 @@ from app.models.user_company_benefit_plan_option import UserCompanyBenefitPlanOp
 
 from app.serializers.company_benefit_plan_option_serializer import \
     CompanyBenefitPlanOptionSerializer
+from app.serializers.enrolled_serializer import EnrolledSerializer
 
 
 class UserCompanyBenefitPlanOptionSerializer(serializers.ModelSerializer):
     company_benefit_plan_option = CompanyBenefitPlanOptionSerializer()
+    enrolled = EnrolledSerializer(many=True)
 
     class Meta:
 
