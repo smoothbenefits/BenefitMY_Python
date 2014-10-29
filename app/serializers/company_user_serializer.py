b9fe5df16c5408ba0d1198eb578de63da0166344
Lin Lin <linlin.cqu@gmail.com>
2014-10-28 23:54:15 -0400

    update user company roles

--- app/serializers/company_user_serializer.py
+++ app/serializers/company_user_serializer.py
@@ -5,13 +5,14 @@ from app.models.company_user import CompanyUser
 
 
 class CompanyUserSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
 
     class Meta:
         model = CompanyUser
         fields = ('id',
                   'company_user_type',
                   'user')
+
+        depth = 1
 
 
 class CompanyUserPostSerializer(serializers.ModelSerializer):
