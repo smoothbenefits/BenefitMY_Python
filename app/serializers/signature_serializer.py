2226ad3545035e2408be807dadb1d9f2e215e4ec
Lin Lin <Linlin.cqu@gmail.com>
2014-11-02 14:02:23 -0500

    minor fix

--- app/serializers/signature_serializer.py
+++ app/serializers/signature_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from app.models.signature_authorization import Signature
+from app.models.signature import Signature
 
 
 class SignatureSerializer(serializers.ModelSerializer):
