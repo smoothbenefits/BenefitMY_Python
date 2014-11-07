f5d1e01496a012f9a32e49e296349daffa9e261a
Lin Lin <linlin.cqu@gmail.com>
2014-11-07 00:17:55 -0500

    add new python module requirement

--- Smoothbenefits/settings.py
+++ Smoothbenefits/settings.py
@@ -49,6 +49,7 @@ AUTHENTICATION_BACKENDS = (
 
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
