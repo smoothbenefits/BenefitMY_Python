8345d5ad476ee51524719cd14eb77d504fd9c67b
Zimeng Chen <eagleofpc@gmail.com>
2014-11-14 13:32:32 -0500

    add the changes to make register work for employee signup

--- app/views/register_view.py
+++ app/views/register_view.py
@@ -1,12 +1,13 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from app.forms import UserForm
-from emailusernames.utils import create_user
+from emailusernames.utils import create_user, get_user, user_exists
+from app.models.user import User
+
 
 def register(request):
     # Like before, get the request's context.
     context = RequestContext(request)
-
     # A boolean value for telling the template whether the registration was successful.
     # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
@@ -36,3 +37,38 @@ def register(request):
 
     # Render the template depending on the context.
     return render_to_response('register.html',{'user_form': user_form, 'registered': registered},context)
+
+def register_employee(request, user_id):
+    context = RequestContext(request)
+    registered = False
+    user_email = ""
+    error_message = ""
+
+    try:
+        employee_user = User.objects.get(pk=user_id)
+        user_email = employee_user.email
+    except User.DoesNotExist:
+            error_message = "We cannot find the user based on the URL. Please contact your HR."
+    
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        confirm_password = request.POST['confirm_password']
+        user_password = request.POST['password']
+        if not user_form.is_valid():
+            error_message = "The Email and Password you have entered is invalid. Please try again."
+
+        if user_password != confirm_password:
+            error_message = "The passwords you have entered do not match. Please try again."
+
+        if error_message:
+            return render_to_response('register.html',{'user_form': user_form, 'registered': registered, 'user_email':user_email, 'errorMessage':error_message},context)
+        else:
+            employee_user.email = request.POST['email']
+            employee_user.set_password(user_password)
+            employee_user.save()
+            return redirect('/login')
+    else:
+        user_form = UserForm()
+
+    # Render the template depending on the context.
+    return render_to_response('register.html',{'user_form': user_form, 'registered': registered, 'user_email':user_email, 'errorMessage':error_message},context)
