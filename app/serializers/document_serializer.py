dc831c6ea0521f195c2764bb9f88ff631062798f
lin lin <linlin.cqu@gmail.com>
2014-11-16 15:30:42 -0500

    document generation works

--- app/serializers/document_serializer.py
+++ app/serializers/document_serializer.py
@@ -2,24 +2,24 @@ from rest_framework import serializers
 
 from document_field_serializer import DocumentFieldSerializer
 from app.models.document import Document
-from app.models.document_type import DocumentType
+from app.models.document_field import DocumentField
 import re
+
 
 class DocumentSerializer(serializers.ModelSerializer):
     content = serializers.SerializerMethodField('generate_content')
+    fields = DocumentFieldSerializer()
 
     def generate_content(self, foo):
         content = foo.template.content
-        field_names = re.findall('{{(.*?)}}', foo.content)
-        fields = DocumentType.object.filter(document_id=foo.id)
+        field_names = re.findall('{{(.*?)}}', content)
+        fields = DocumentField.objects.filter(document_id=foo.id)
+        fields_dict = {f.name: f.value for f in fields}
         for f in field_names:
-            content.replace("{{%s}}" % f, "{{%}}" % fields[f])
+            if f in fields_dict:
+                content = content.replace("{{%s}}" % f, fields_dict[f])
 
         return content
-
-
-
-    fields = DocumentFieldSerializer()
 
     class Meta:
         model = Document
