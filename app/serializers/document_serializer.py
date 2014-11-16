fbe8f3c701948813fd88c0c1851088b5f07d00b0
lin lin <linlin.cqu@gmail.com>
2014-11-15 23:45:01 -0500

    generate document content

--- app/serializers/document_serializer.py
+++ app/serializers/document_serializer.py
@@ -2,17 +2,31 @@ from rest_framework import serializers
 
 from document_field_serializer import DocumentFieldSerializer
 from app.models.document import Document
-
+from app.models.document_type import DocumentType
+import re
 
 class DocumentSerializer(serializers.ModelSerializer):
+    content = serializers.SerializerMethodField('generate_content')
+
+    def generate_content(self, foo):
+        content = foo.template.content
+        field_names = re.findall('{{(.*?)}}', foo.content)
+        fields = DocumentType.object.filter(document_id=foo.id)
+        for f in field_names:
+            content.replace("{{%s}}" % f, "{{%}}" % fields[f])
+
+        return content
+
+
+
     fields = DocumentFieldSerializer()
 
     class Meta:
         model = Document
         fields = ('id',
                   'name',
-                  'content',
                   'edited',
+                  'content',
                   'company',
                   'user',
                   'template',
@@ -20,4 +34,3 @@ class DocumentSerializer(serializers.ModelSerializer):
                   'signature',
                   'fields')
         depth = 1
-
