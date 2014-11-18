dd9248e070a70f346452c04b667eb6da33fa9428
lin lin <llin@vertica.com>
2014-11-17 23:00:07 -0500

    add user benefits works

--- app/views/user_company_benefit_plan_option_view.py
+++ app/views/user_company_benefit_plan_option_view.py
@@ -1,12 +1,13 @@
 from rest_framework.views import APIView
 from django.http import Http404
 
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from app.models.enrolled import Enrolled
 from app.models.user_company_benefit_plan_option import UserCompanyBenefitPlanOption
 from app.serializers.user_company_benefit_plan_option_serializer import (
     UserCompanyBenefitPlanOptionSerializer)
-
 
 
 class UserCompanyBenefitPlanOptionView(APIView):
@@ -21,6 +22,12 @@ class UserCompanyBenefitPlanOptionView(APIView):
         serializer = UserCompanyBenefitPlanOptionSerializer(plans, many=True)
         return Response({'benefits': serializer.data})
 
+    def post(self, request, pk, format=None):
+        _add_user_benefits(request, pk)
+        benefits = UserCompanyBenefitPlanOption.objects.filter(user=pk)
+        serializer = UserCompanyBenefitPlanOptionSerializer(benefits, many=True)
+        return Response({'benefits': serializer.data})
+
 
 def _add_user_benefits(request, pk):
     for benefit in request.DATA['benefits']:
@@ -29,21 +36,13 @@ def _add_user_benefits(request, pk):
             benefit_id=benefit['benefit']['id'])
         u.save()
         for e in enroll_list:
-            enrolled = Enrolled(user_company_benefit_plan_option=u.id,
-                                person=e)
+            enrolled = Enrolled(user_company_benefit_plan_option=u,
+                                person_id=e)
             enrolled.save()
 
 
-@api_view(['POST'])
-def user_add_benefits(request, pk):
-    _add_user_benefits(request, pk)
-    benefits = UserCompanyBenefitPlanOption.objects.filter(user=pk)
-    serializer = UserCompanyBenefitPlanOptionSerializer(benefits, many=True)
-    return Response({'benefits': serializer.data})
-
-
 @api_view(['PUT'])
-def user_select_benefits(request, pk, pc):
+def user_update_benefits(request, pk, pc):
     benefits = UserCompanyBenefitPlanOption.objects.filter(user=pk)
     for b in benefits:
         if b.benefit.company_id == pc:
