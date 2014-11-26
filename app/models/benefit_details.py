b491e73f6f36aa7bd3926a6e1ef717806602a03b
lin lin <llin@vertica.com>
2014-11-25 23:54:26 -0500

    add benefit details view

--- app/models/benefit_details.py
+++ app/models/benefit_details.py
@@ -1,7 +1,7 @@
 from django.db import models
 from benefit_policy_key import BenefitPolicyKey
 from benefit_policy_type import BenefitPolicyType
-from company_benefit_plan_option import ComapanyBenefitPlanOption
+from company_benefit_plan_option import CompanyBenefitPlanOption
 
 class BenefitDetails(models.Model):
     value = models.CharField(max_length=1024)
@@ -15,7 +15,7 @@ class BenefitDetails(models.Model):
         blank=True,
         null=True)
 
-    benefit_plan = models.ForeignKey(ComapanyBenefitPlanOption,
+    benefit_plan = models.ForeignKey(CompanyBenefitPlanOption,
                                      related_name="benefit_details",
                                      blank=True,
                                      null=True)
