c139f85b58429b2d61f0c043c5086fdd6dc909b1
Zimeng Chen <eagleofpc@gmail.com>
2014-12-04 22:17:34 -0500

    add the changes for benefit details input change

--- app/static/js/controllers/BrokerControllers.js
+++ app/static/js/controllers/BrokerControllers.js
@@ -177,3 +177,193 @@ var addClientController = brokersControllers.controller('addClientController', [
     }
   }
 ]);
+
+var benefitInputDetailsController = brokersControllers.controller('benefitInputDetailsController',
+    ['$scope',
+     '$location',
+     '$routeParams',
+     'benefitListRepository',
+     'benefitDetailsRepository',
+     function benefitInputDetailsController ($scope,
+                                             $location,
+                                             $routeParams,
+                                             benefitListRepository,
+                                             benefitDetailsRepository){
+      $scope.clientId = parseInt($routeParams.client_id);
+      $scope.benefitId = parseInt($routeParams.benefit_id);
+      $scope.policyKeyArray = [
+        {position:0, name:'Individual Deductables'},
+        {position:1, name:'Family Deductables'},
+        {position:2, name:'Hospital-Inpatient'},
+        {position:3, name:'Out-patient Day Surgery'},
+        {position:4, name:'MRI/CT/PET Scans'},
+        {position:5, name:'Lab work/X-Ray'},
+        {position:6, name:'Chiropractic'},
+        {position:7, name:'Prescription Drugs-30 days'},
+        {position:8, name:'Mail order drugs-90 days'},
+        {position:9, name:'Annual Maximum'},
+        {position:10, name:'Primary Care Physician required'}];
+        
+      benefitListRepository.get({clientId:$scope.clientId})
+        .$promise.then(function(response){
+          $scope.benefit = _.findWhere(response.benefits, {id:$scope.benefitId});
+        });
+      $scope.benefitDetailArray = [];
+      $scope.columnCount = 1;
+      var benefitDetailBody = $('#details_container_table_body');
+
+      var getPolicyTypeObjectById = function(policyTypeId){
+        return _.findWhere($scope.benefitDetailArray, {policy_type_id: policyTypeId});
+      };
+
+      var createInputElement = function(policyTypeId, optionKey, placeHolderText){
+          var valueInput = $(document.createElement('input'));
+          valueInput.attr('type', 'text');
+          valueInput.attr('placeholder', placeHolderText);
+          valueInput.attr('key', optionKey);
+          valueInput.attr('policy-type-id', policyTypeId);
+          valueInput.on('keypress', changeInputKeyPress);
+          return valueInput;
+      };
+
+      var tryCreateNewPolicyTypeDataSet = function(policyTypeId, policyTypeValue){
+        var existingPolicyType = getPolicyTypeObjectById(policyTypeId);
+        if(!existingPolicyType){
+          //There is no existing policyType, create a new policyType
+          var policyType = {policy_type:policyTypeValue, policy_type_id: policyTypeId, policy_array:[]};
+          $scope.benefitDetailArray.push(policyType);
+          $scope.columnCount ++;
+          return true;
+        }
+        else{
+          existingPolicyType.policy_type = policyTypeValue;
+          return false;
+        }
+      };
+
+      var updateTableWithNewPolicyType = function(target, policyTypeId){
+        var thElement = target.parent();
+        var thContainer = thElement.parent();
+        var newTh = $(document.createElement('th'));
+        //First create the new th on the right hand side
+        var newPolicyTypeLinkContainer = $(document.createElement('div'));
+        var newPolicyTypeLink = $(document.createElement('a'));
+        newPolicyTypeLink.on('click', handleEditElement)
+        newPolicyTypeLink.html('Add New Plan');
+        newPolicyTypeLinkContainer.append(newPolicyTypeLink);
+        newTh.append(newPolicyTypeLinkContainer);
+        thContainer.append(newTh);
+
+        //update each row with the input
+        var tableRows = benefitDetailBody.children('tr');
+        _.each(tableRows, function(row){
+          var rowKey = $(row).attr('option-key');
+          if(rowKey){
+            var tableCellArray = $(row).children('td')
+            var lastTableCell = tableCellArray[tableCellArray.length -1];
+            $(lastTableCell).append(createInputElement(policyTypeId, rowKey, 'Add option value'));
+            var newTableCell = $(document.createElement('td'));
+            $(row).append(newTableCell);
+          }
+        });
+      };
+
+
+      var tryCreateNewPolicyValue = function(policyTypeId, optionKey, policyValue){
+        var policyTypeObject = getPolicyTypeObjectById(policyTypeId);
+          var policyPair = _.findWhere(policyTypeObject.policy_array, {policy_key:optionKey});
+          if(policyPair){
+            policyPair.policy_value = policyValue;
+            return false;
+          }
+          else{
+            policyPair = {policy_key:optionKey, policy_value:policyValue};
+            policyTypeObject.policy_array.push(policyPair);
+            return true;
+          }
+      };
+
+      var deletePolicyType = function(event){
+
+      };
+
+      var createValueDiv = function(policyTypeId, optionKey, content, showDelete){
+          var saveTextContainer = $(document.createElement('div'));
+          saveTextContainer.addClass('editable-container');
+          var contentSpan = $(document.createElement('span'));
+          contentSpan.attr('policy-type-id', policyTypeId);
+          if(optionKey){
+            contentSpan.attr('key', optionKey);
+          }
+          contentSpan.append(content);
+          contentSpan.on('click', handleEditElement);
+          saveTextContainer.append(contentSpan);
+          if(showDelete){
+            //add delete icon to this div
+            var removeSpan = $(document.createElement('a'));
+            removeSpan.append('del');
+            removeSpan.attr('policy-type-id', policyTypeId);
+            removeSpan.on('click', deletePolicyType);
+            saveTextContainer.append(removeSpan);
+          }
+          return saveTextContainer;
+      };
+
+
+      var changeInputKeyPress = function(event){
+          if(event.charCode == 13){
+            var inputVal = $(event.target).val();
+            var optionKey = $(event.target).attr('key');
+            var policyTypeId = $(event.target).attr('policy-type-id');
+            var targetContainer = $(event.target).parent();
+            var showDeleteIcon = false;
+            //Populate the data set
+            if(targetContainer[0].tagName ==='TH'){
+              //This is another new option set.
+              if(tryCreateNewPolicyTypeDataSet(policyTypeId, inputVal)){
+                updateTableWithNewPolicyType($(event.target), policyTypeId);
+              }
+              showDeleteIcon = true;
+            }
+            else if(policyTypeId && optionKey){
+              //this is just the option type value
+              tryCreateNewPolicyValue(policyTypeId, optionKey, inputVal);
+            }
+            targetContainer.empty();
+            targetContainer.append(createValueDiv(policyTypeId, optionKey, inputVal, showDeleteIcon));
+
+          }
+      };
+
+      var lostFocusHandler = function(blurEvent){
+        var textInput = $(blurEvent.target);
+        var curPolicyTypeId = textInput.attr('policy-type-id');
+        var curOptionKey = textInput.attr('key');
+        var container = textInput.parent();
+        var originalText = textInput.attr('placeholder');
+        container.empty();
+        container.append(createValueDiv(curPolicyTypeId, curOptionKey, originalText));
+      };
+
+      function handleEditElement (clickEvent){
+        var container = $(clickEvent.target).parent().parent();
+        var placeHolderText = $(clickEvent.target).html();
+        var curPolicyTypeId = $(clickEvent.target).attr('policy-type-id');
+        if(!curPolicyTypeId){
+          curPolicyTypeId = $scope.columnCount;
+        }
+        var curOptionKey = $(clickEvent.target).attr('key');
+        var typeTextInput = createInputElement(curPolicyTypeId, curOptionKey, placeHolderText);
+        typeTextInput.on('blur', lostFocusHandler)
+        container.empty();
+        container.append(typeTextInput);
+        typeTextInput.focus();
+      };
+      
+
+      $scope.handleElementEvent = handleEditElement;
+
+      $scope.addBenefitDetail = function(){
+
+      };
+}]);
