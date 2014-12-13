674debff14dc63df581aa1446d7e0a4ab16c232b
lin lin <llin@vertica.com>
2014-12-13 09:57:24 -0500

    company user view login required

--- app/views/company_user_view.py
+++ app/views/company_user_view.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from app.models.company_user import CompanyUser
 from app.serializers.company_user_serializer import (
     CompanyUserSerializer)
+from view_mixin import *
 
 
-class CompanyUserView(APIView):
+class CompanyUserView(APIView, LoginRequiredMixin):
     def get_companies(self, pk):
         try:
             return CompanyUser.objects.filter(company=pk)
@@ -17,24 +18,26 @@ class CompanyUserView(APIView):
     def get(self, request, pk, format=None):
         companies = self.get_companies(pk)
         serializer = CompanyUserSerializer(companies, many=True)
-        return Response({'user_roles':serializer.data})
+        return Response({'user_roles': serializer.data})
 
 
-class CompanyEmployeeCountView(APIView):
+class CompanyEmployeeCountView(APIView, LoginRequiredMixin):
 
     def get(self, request, pk, format=None):
         return Response({'employees_count':
             len(CompanyUser.objects.filter(company=pk,
                                        company_user_type='employee'))})
 
-class CompanyBrokerCountView(APIView):
+
+class CompanyBrokerCountView(APIView, LoginRequiredMixin):
 
     def get(self, request, pk, format=None):
         return Response({'brokers_count':
             len(CompanyUser.objects.filter(company=pk,
                                        company_user_type='broker'))})
 
-class BrokerCompanyCountView(APIView):
+
+class BrokerCompanyCountView(APIView, LoginRequiredMixin):
 
     def get(self, request, pk, format=None):
         return Response({'companies_count':
