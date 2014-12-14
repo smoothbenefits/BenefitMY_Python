be23dfc465f8a176f35f960809b1d7b2b404583a
Zimeng Chen <eagleofpc@gmail.com>
2014-12-14 09:13:21 -0500

    BM-13 Generate cobra document for all my employee

--- app/static/js/services/services.js
+++ app/static/js/services/services.js
@@ -524,3 +524,28 @@ benefitmyService.factory('benefitDisplayService',
             });
     };
 }]);
+
+benefitmyService.factory(
+  'documentTypeService',
+  ['documentRepository',
+   function(documentRepository){
+    return {
+      getDocumentTypeById: function(companyId, documentTypeId, serviceCallBack){
+        documentRepository.type.get({companyId:companyId})
+          .$promise.then(function(docTypeResponse){
+            var docType = _.findWhere(docTypeResponse.document_types, {id:documentTypeId});
+            if(serviceCallBack){
+              serviceCallBack(docType);
+            }
+          });
+      },
+      getDocumentTypes: function(companyId, serviceCallBack){
+        documentRepository.type.get({companyId:companyId})
+          .$promise.then(function(response){
+            if(serviceCallBack){
+              serviceCallBack(response.document_types);
+            }
+          });
+      }
+    };
+  }]);
