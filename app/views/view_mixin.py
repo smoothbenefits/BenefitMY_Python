f1f1c371aaacd71f18b7eec1fbeb8a5dccc6ddbf
lin lin <llin@vertica.com>
2014-12-16 23:19:33 -0500

    add the check broker permission function

--- app/views/view_mixin.py
+++ app/views/view_mixin.py
@@ -14,11 +14,22 @@ login_required = login_required
 
 
 def is_employer(user_id, company_id):
-    """ check if a user is the employer of a company"""
+    """ check if a user is the admin of a company"""
     try:
         CompanyUser.objects.get(user=user_id,
                                 company=company_id,
                                 company_user_type='admin')
+        return True
+    except CompanyUser.DoesNotExist:
+        return False
+
+
+def is_broker(user_id, company_id):
+    """ check if a user is the broker of a company"""
+    try:
+        CompanyUser.objects.get(user=user_id,
+                                company=company_id,
+                                company_user_type='broker')
         return True
     except CompanyUser.DoesNotExist:
         return False
