da24c69f8aa6be3bf6141796a85f104442202c00
Zimeng Chen <eagleofpc@gmail.com>
2014-12-28 19:50:09 -0500

    add the default content for the document types database scripts into our code base

---
+++ data_migration/app_documenttype_EA_content.sql
@@ -0,0 +1,290 @@
+update app_documenttype
+set default_content = 'This Employment Agreement is entered into as of the date of the last
+signature affixed hereto, by and between {{CompanyName}}, a 
+Massachusetts corporation ("the Company"), and 
+{{EmployeeName}}("Employee").
+In consideration of the mutual promises and covenants set forth 
+herein, and other good and valuable consideration, the sufficiency of 
+which is hereby acknowledged, {{CompanyName}} and Employee 
+hereby agree as follows: 
+1 Position of Employment. The Company will employ the Employee in 
+the position of {{EmployeeTitle}} of the Company and, in that 
+position, Employee will report to the {{EmployeeTitle}} of 
+{{CompanyName}}.  {{CompanyName}} retains the right to 
+change Employee''s title, duties, and reporting relationships as 
+may be determined to be in the best interests of the Company; 
+provided, however, that any such change in Employee''s duties 
+shall be consistent with Employee''s training, experience, and 
+qualifications.
+The terms and conditions of the Employee''s employment shall, 
+to the extent not addressed or described in this Employment 
+Agreement, be governed by {{CompanyName}}''s Policies and 
+Procedures Manual and existing practices. In the event of a 
+conflict between this Employment Agreement and the Policies 
+and Procedures Manual or existing practices, the terms of this 
+Agreement shall govern.  
+2 Term of Employment. Employee''s employment with 
+{{CompanyName}} Company shall begin on 
+{{EmployeeStartDate}} and the time continued employment 
+shall be on an "at will" basis, unless:
+a Employee''s employment is terminated by either party in 
+accordance with the terms of Section 5 of this 
+Employment Agreement; or 
+b Such term of employment is extended or shortened by a 
+subsequent agreement duly executed by each of the 
+parties to this Employment Agreement, in which case 
+such employment shall be subject to the terms and 
+conditions contained in the subsequent written 
+agreement. 
+3 Compensation and Benefits. 
+a Base Salary. Employee shall be paid a base salary of 
+{{EmployeeSalary}} annually ("Base Salary"), subject to 
+applicable federal, state, and local withholding, such Base 
+Salary to be paid to Employee in the same manner and 
+on the same payroll schedule in which all 
+{{CompanyName}} employees receive payment. Any 
+increases in Employee''s Base Salary for years beyond 
+the first year of Employee''s employment shall be in the 
+sole discretion of {{CompanyName}} Company 
+management, and nothing herein shall be deemed to 
+require any such increase. 
+b Incentive and Deferred Compensation. Employee shall be 
+eligible to participate in all incentive and deferred 
+compensation programs available to other executives or 
+officers of {{CompanyName}} Company, such 
+participation to be in the same form, under the same 
+terms, and to the same extent that such programs are 
+made available to other such executives or officers.
+Nothing in this Employment Agreement shall be deemed 
+to require the payment of bonuses, awards, or incentive 
+compensation to Employee if such payment would not 
+otherwise be required under the terms of 
+{{CompanyName}} Company''s incentive compensation 
+programs. 
+c Employee Benefits. Employee shall be eligible to participate 
+in all employee benefit plans, policies, programs, or 
+perquisites in which other {{CompanyName}} Company 
+executive or officers participate, including the 
+{{CompanyName}} Company Stock Option program. The 
+terms and conditions of Employee''s participation in 
+{{CompanyName}} Company''s employee benefit plans, 
+policies, programs, or perquisites shall be governed by 
+the terms of each such plan, policy, or program. 
+4 Duties and Performance. The Employee acknowledges and agrees 
+that he is being offered a position of employment by the 
+Company with the understanding that the Employee possesses 
+a unique set of skills, abilities, and experiences which will 
+benefit the Company, and he agrees that his continued 
+employment with the Company, whether during the term of this 
+Employment Agreement or thereafter, is contingent upon his 
+successful performance of his duties in his position as noted 
+above, or in such other position to which he may be assigned. 
+a General Duties. 
+i Employee shall render to the very best of 
+Employee''s ability, on behalf of the Company, 
+services to and on behalf of the Company, and shall 
+undertake diligently all duties assigned to him by the 
+Company. 
+ii Employee shall devote his full time, energy and skill 
+to the performance of the services in which the 
+Company is engaged, at such time and place as the 
+Company may direct. Employee shall not 
+undertake, either as an owner, director, 
+shareholder, employee or otherwise, the 
+performance of services for compensation (actual or 
+expected) for any other entity without the express 
+written concert of the President or the Board of 
+Directors. 
+iii Employee shall faithfully and industriously assume 
+and perform with skill, care, diligence and attention 
+all responsibilities and duties connected with his 
+employment on behalf of the Company.
+iv Employee shall have no authority to enter into any 
+contracts binding upon the Company, or to 
+deliberately create any obligations on the part of the 
+Company, except as may be specifically authorized 
+by the President or Board of Directors of 
+{{CompanyName}} Company. 
+5 Termination of Employment. Employee''s employment with the 
+Company may be terminated, prior to the expiration of the term 
+of this Employment Agreement, in accordance with any of the 
+following provisions: 
+a Termination by Employee. The Employee may terminate his 
+employment at any time during the course of this 
+agreement by giving 1 months’ notice in writing to the 
+President of {{CompanyName}} Company. During the 
+notice period, Employee must fulfill all his duties and 
+responsibilities set forth above and use his best efforts to 
+train and support his replacement, if any. Failure to 
+comply with this requirement may result in Termination for 
+Cause described below, but otherwise Employee''s salary 
+and benefits will remain unchanged during the notification 
+period. 
+b Termination by the Company Without Cause. 
+{{CompanyName}} Company may terminate Employee''s 
+employment at any time during the course of this 
+agreement by giving 1 weeks/months'' notice in writing to 
+the Employee. During the notice period, Employee must 
+fulfill all of Employee''s duties and responsibilities set forth 
+above and use Employee''s best efforts to train and 
+support Employee''s replacement, if any. Failure of 
+Employee to comply with this requirement may result in 
+Termination for Cause described below, but otherwise 
+Employee''s salary and benefits will remain unchanged 
+during the notification period. {{CompanyName}}  
+Company, may, in its sole discretion, give Employee  
+severance pay in the amount of the remaining notice  
+period in lieu of actual employment, and nothing herein  
+shall require Company to maintain employee in active  
+employment for the duration of the notice period. 
+c Termination by the Company For Cause. The Company may,  
+at any time and without notice, terminate the Employee  
+for "cause". Termination by the Company of the  
+Employee for "cause" shall include but not be limited to  
+termination based on any of the following grounds: (a)  
+failure to perform the duties of the Employee''s position in  
+a satisfactory manner; (b) fraud, misappropriation,  
+embezzlement or acts of similar dishonesty; (c) conviction  
+of a felony involving moral turpitude; (d) illegal use of  
+drugs or excessive use of alcohol in the workplace; (e)  
+intentional and willful misconduct that may subject the  
+Company to criminal or civil liability; (f) breach of the  
+Employee''s duty of loyalty, including the diversion or  
+usurpation of corporate opportunities properly belonging  
+to the Company; (g) willful disregard of Company policies  
+and procedures; (h) breach of any of the material terms of  
+this Agreement; and (i) insubordination or deliberate  
+refusal to follow the instructions of the President of  
+{{CompanyName}} Company. 
+d Termination By Death or Disability. The Employee''s  
+employment and rights to compensation under this  
+Employment Agreement shall terminate if the Employee is  
+unable to perform the duties of his position due to death  
+or disability lasting more than 90 days, and the  
+Employee''s heirs, beneficiaries, successors, or assigns  
+shall not be entitled to any of the compensation or  
+benefits to which Employee is entitled under this  
+Agreement, except: (a) to the extent specifically provided  
+in this Employment Agreement (b) to the extent required  
+by law; or (c) to the extent that such benefit plans or  
+policies under which Employee is covered provide a  
+benefit to the Employee''s heirs, beneficiaries, successors,  
+or assigns. 
+6 Confidentiality. Employee agrees that at all times during  
+Employee''s employment and following the conclusion of  
+Employee''s employment, whether voluntary or involuntary,  
+Employee will hold in strictest confidence and not disclose  
+Confidential Information (as defined below) to anyone who is  
+not also an employee of the Company or to any employee of  
+the Company who does not also have access to such  
+Confidential Information, without express written authorization  
+of the President of the Company. "Confidential Information"  
+shall mean any trade secrets or Company proprietary  
+information, including but not limited to manufacturing  
+techniques, processes, formulas, customer lists, inventions,  
+experimental developments, research projects, operating  
+methods, cost, pricing, financial data, business plans and  
+proposals, data and information the Company receives in  
+confidence from any other party, or any other secret or  
+confidential matters of the Company. Additionally, Employee  
+will not use any Confidential Information for Employee''s own  
+benefit or to the detriment of the Company during Employee''s  
+employment or thereafter. Employee also certifies that  
+employment with the Company does not and will not breach  
+any agreement or duty that Employee has to anyone  
+concerning confidential information belonging to others. 
+7 Noncompetition. To the fullest extent permitted by applicable law,  
+the terms of the Noncompetition Agreement executed by the  
+Employee are incorporated by reference into this Employment  
+Agreement and are made a part hereto as if they appeared in  
+this Employment Agreement itself. 
+8 Expenses. The Company shall pay or reimburse Employee for any  
+expenses reasonably incurred by him in furtherance of his  
+duties hereunder, including expenses for entertainment, travel,  
+meals and hotel accommodations, upon submission by him of  
+vouchers or receipts maintained and provided to the Company  
+in compliance with such rules and policies relating thereto as  
+the Company may from time to time adopt. 
+9 General Provisions. 
+a Notices. All notices and other communications required or  
+permitted by this Agreement to be delivered by  
+{{CompanyName}} Company or Employee to the other  
+party shall be delivered in writing to the address shown  
+below, either personally, by facsimile transmission or by  
+registered, certified or express mail, return receipt  
+requested, postage prepaid, to the address for such party  
+specified below or to such other address as the party may  
+from time to time advise the other party, and shall be  
+deemed given and received as of actual personal  
+delivery, on the first business day after the date of  
+b Amendments and Termination; Entire Agreement. This  
+delivery shown on any such facsimile transmission or  
+upon the date or actual receipt shown on any return  
+receipt if registered, certified or express mail is used, as  
+the case may be. 
+{{CompanyName}} Company:  
+[Attention: {{CompanyHRManager}}] 
+Employee: 
+{{EmployeeName}} 
+       
+Agreement may not be amended or terminated except by  
+a writing executed by all of the parties hereto. This  
+Agreement constitutes the entire agreement of  
+{{CompanyName}} Company and Employee relating to  
+the subject matter hereof and supersedes all prior oral  
+and written understandings and agreements relating to  
+such subject matter. 
+c Successors and Assigns. The rights and obligations of the  
+parties hereunder are not assignable to another person  
+without prior written consent; provided, however, that  
+{{CompanyName}} Company, without obtaining  
+Employee''s consent, may assign its rights and obligations  
+hereunder to a wholly-owned subsidiary and provided  
+further that any post-employment restrictions shall be  
+assignable by {{CompanyName}} to any entity which  
+purchases all or substantially all of the Company''s assets. 
+d Severability; Provisions Subject to Applicable Law. All  
+provisions of this Agreement shall be applicable only to  
+the extent that they do not violate any applicable law, and  
+are intended to be limited to the extent necessary so that  
+they will not render this Agreement invalid, illegal or  
+unenforceable under any applicable law. If any provision  
+of this Agreement or any application thereof shall be held  
+to be invalid, illegal or unenforceable, the validity, legality  
+and enforceability of other provisions of this Agreement or  
+of any other application of such provision shall in no way  
+be affected thereby. 
+e Waiver of Rights. No waiver by {{CompanyName}} Company  
+or Employee of a right or remedy hereunder shall be  
+deemed to be a waiver of any other right or remedy or of  
+any subsequent right or remedy of the same kind. 
+f Definitions; Headings; and Number. A term defined in any  
+part of this Employment Agreement shall have the defined  
+meaning wherever such term is used herein. The  
+headings contained in this Agreement are for reference  
+purposes only and shall not affect in any manner the  
+meaning or interpretation of this Employment Agreement.  
+Where appropriate to the context of this Agreement, use  
+of the singular shall be deemed also to refer to the plural,  
+and use of the plural to the singular.  
+g Counterparts. This Agreement may be executed in separate  
+counterparts, each of which shall be deemed an original  
+but both of which taken together shall constitute but one  
+and the same instrument. 
+h Governing Laws and Forum. This Agreement shall be  
+governed by, construed, and enforced in accordance with  
+the laws of the Commonwealth of Massachusetts. The  
+parties hereto further agree that any action brought to  
+enforce any right or obligation under this Agreement shall  
+be subject to the exclusive jurisdiction of the courts of the  
+Commonwealth of Massachusetts. 
+IN WITNESS WHEREOF, {{CompanyName}} Company and  
+Employee have executed and delivered this Agreement as of the date  
+written below. 
+
+
+                                    {{CompanyName}} 
+_________________________           By: ___________________________ 
+John A. Doe                         Name: _________________________ 
+                                    Title: __________________________'
+where name='Employment Agreement' 
