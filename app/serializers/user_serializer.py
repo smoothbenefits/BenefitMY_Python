62823a06e936225d5b505ec1ca288ae5e91f0ec0
lin lin <llin@vertica.com>
2014-12-31 19:31:12 -0500

    remove first/last name from user table

--- app/serializers/user_serializer.py
+++ app/serializers/user_serializer.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 from app.models.user import User
+from app.models.person import Person
 from app.serializers.person_serializer import PersonSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """ we only use person model first/last name
+    """
+    first_name = serializers.SerializerMethodField('get_first_name')
+    last_name = serializers.SerializerMethodField('get_last_name')
+
+    def get_first_name(self, foo):
+        p = Person.objects.get(user=foo.id)
+        return p.first_name
+
+    def get_last_name(self, foo):
+        p = Person.objects.get(user=foo.id)
+        return p.last_name
 
     class Meta:
         model = User
