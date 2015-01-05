ad5db484033d13fd13024ff5a544c98087eca4a1
lin lin <llin@vertica.com>
2015-01-04 23:24:17 -0500

    BM-87: dynamic url address for email

--- Smoothbenefits/settings.py
+++ Smoothbenefits/settings.py
@@ -13,6 +13,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
+SITE_URL = "https://demo.benefitmy.com/"
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
