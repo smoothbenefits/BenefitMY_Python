4da306bbaea6b0bd4e01d5fec0483af8f0bcd7a3
Zimeng Chen <eagleofpc@gmail.com>
2015-01-08 22:59:05 -0500

    BM-139 refactor the employee onboarding logic with the personal info together

--- app/static/js/services/services.js
+++ app/static/js/services/services.js
@@ -617,3 +617,70 @@ benefitmyService.factory(
       }
     };
   }]);
+
+benefitmyService.factory('selfInfoService',
+  ['employeeFamily',
+    function(employeeFamily){
+      return{
+        getSelfInfo: function(uId, retrievedCallBack){
+          employeeFamily.get({userId:uId})
+            .$promise.then(function(userPerson){
+              var selfPerson = _.findWhere(userPerson.family, {relationship:'self'});
+              if(selfPerson){
+                if(selfPerson.phones && selfPerson.phones.length > 0){
+                  selfPerson.phone = selfPerson.phones[0];
+                }
+                if(selfPerson.addresses && selfPerson.addresses.length > 0){
+                  selfPerson.address = selfPerson.addresses[0];
+                }
+                if(retrievedCallBack){
+                  retrievedCallBack(selfPerson);
+                }
+              }
+              else if(retrievedCallBack){
+                retrievedCallBack(null);
+              }
+            });
+        },
+
+        saveSelfInfo: function(uId, viewInfo, success, error){
+          var mapUserPerson = function(viewPerson){
+            var birth_date = moment(viewPerson.birth_date);
+            var apiUserPerson = viewPerson;
+            apiUserPerson.birth_date = birth_date.format('YYYY-MM-DD')
+            apiUserPerson.addresses = [];
+            viewPerson.address.address_type = 'home';
+            viewPerson.address.state = viewPerson.address.state.toUpperCase();
+            apiUserPerson.addresses.push(viewPerson.address);
+            if(apiUserPerson.phones && apiUserPerson.phones.length > 0){
+              apiUserPerson.phones[0].number = viewPerson.phone.number;
+            }
+            else{
+              apiUserPerson.phones = [];
+              apiUserPerson.phones.push({phone_type:'home', number:viewPerson.phone.number});
+            }
+            if(!apiUserPerson.person_type){
+              apiUserPerson.person_type = 'primary_contact';
+            }
+            if(!apiUserPerson.relationship){
+              apiUserPerson.relationship = 'self';
+            }
+            return apiUserPerson;
+          };
+
+          var newUserInfo = mapUserPerson(viewInfo);
+          employeeFamily.save({userId:uId}, newUserInfo,
+          function(response){
+            if(success){
+              success(response);
+            }
+          }, function(errorResponse){
+            if(error){
+              error(errorResponse);
+            }
+          });
+        }
+      };
+
+
+}]);
