b62a3d06746462557f3721d469bb7ad4c64532dd
Lin Lin <linlin.cqu@gmail.com>
2015-01-26 19:45:41 -0500

    minor people test case update

--- app/views/tests/test_person_view.py
+++ app/views/tests/test_person_view.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+
 class PersonTestCase(TestCase):
     # your fixture files here
-    fixtures = ['24_person', '10_company', '23_auth_user']
-
+    fixtures = ['24_person', '10_company', '23_auth_user', '11_address',
+                '12_phone']
 
     def test_get_person(self):
         response = self.client.get(reverse('people_api', kwargs={'pk': 1}))
         self.assertIsNotNone(response)
         self.assertEqual(response.status_code, 200)
-        
