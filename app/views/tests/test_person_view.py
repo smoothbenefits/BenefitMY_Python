2a2ccdf961a4bc945de153d34a408cd8822c5537
Lin Lin <linlin.cqu@gmail.com>
2015-01-28 11:05:58 -0500

    person delete view test

--- app/views/tests/test_person_view.py
+++ app/views/tests/test_person_view.py
@@ -20,3 +20,12 @@ class PersonTestCase(TestCase):
                          '02141')
         self.assertEqual(result['person']['phones'][0]['number'],
                          '6078980780')
+
+    def test_del_person(self):
+        response = self.client.delete(reverse('people_api',
+                                              kwargs={'pk': 1}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content)
+        self.assertEqual(result['deleted_person']['first_name'], 'John')
+        self.assertEqual(result['deleted_person']['last_name'], 'Hancock')
