38abe4f2b615f1c375f5bc625dcd2500f2b78788
Zimeng Chen <eagleofpc@gmail.com>
2015-01-30 13:17:17 -0500

    Add the unit tests for user and login view

--- app/urls.py
+++ app/urls.py
@@ -67,10 +67,9 @@ urlpatterns = patterns('app.views',
 
     url(r'^%s/document_types/?$' % PREFIX, DocumentTypeView.as_view(), name='document_type_api'),
 
-
-    url(r'^%s/users/?$' % PREFIX, UsersView.as_view(), name='users_api'),
-    url(r'^%s/users/current/?$' % PREFIX, CurrentUserView.as_view(), name='current_user_api'),
-    url(r'^%s/users/(?P<pk>[0-9]+)/?$' % PREFIX, UserView.as_view(), name='user_api'),
+    url(r'^%s/users/?$' % PREFIX, UsersView.as_view(), name='all_users'),
+    url(r'^%s/users/current/?$' % PREFIX, CurrentUserView.as_view(), name='current_user'),
+    url(r'^%s/users/(?P<pk>[0-9]+)/?$' % PREFIX, UserView.as_view(), name='user_by_id'),
     url(r'^%s/users/(?P<pk>[0-9]+)/family/?$' % PREFIX, UserFamilyView.as_view(), name='user_family_api'),
     url(r'^%s/users/(?P<pk>[0-9]+)/documents/?$' % PREFIX, UserDocumentView.as_view()),
     url(r'^%s/users/(?P<pk>[0-9]+)/benefits/?$' % PREFIX,
