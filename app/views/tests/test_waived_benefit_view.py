126ec05ddb4aedc869128e55ba55c838a244e15f
Lin Lin <linlin.cqu@gmail.com>
2015-01-30 20:33:30 -0500

    user waived benefit view test

---
+++ app/views/tests/test_waived_benefit_view.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+import json
+
+
+class WaivedBenefitTestCase(TestCase):
+    # your fixture files here
+    fixtures = ['31_company_benefit_plan_option', '21_benefit_plan', '10_company', '13_benefit_type',
+                'waived_benefit', '23_auth_user']
+
+    def test_user_waived_benefit(self):
+        response = self.client.get(reverse('user_waived_benefit_api',
+                                           kwargs={'pk': 1}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content)
+        self.assertEqual(type(result), list)
+        self.assertEqual(result[0]['company']['name'], 'Startup.Com')
+        self.assertEqual(result[0]['benefit_type']['name'], 'Vision')
+        self.assertEqual(result[0]['user']['first_name'], 'John')
