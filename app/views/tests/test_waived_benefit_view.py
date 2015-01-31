7f9f7f21a78ea15524fd4f5b3ad290be6c289437
Lin Lin <linlin.cqu@gmail.com>
2015-01-30 20:36:58 -0500

    company waived benefit view test

--- app/views/tests/test_waived_benefit_view.py
+++ app/views/tests/test_waived_benefit_view.py
@@ -18,3 +18,15 @@ class WaivedBenefitTestCase(TestCase):
         self.assertEqual(result[0]['company']['name'], 'Startup.Com')
         self.assertEqual(result[0]['benefit_type']['name'], 'Vision')
         self.assertEqual(result[0]['user']['first_name'], 'John')
+
+
+    def test_company_waived_benefit(self):
+        response = self.client.get(reverse('company_waived_benefit_api',
+                                           kwargs={'pk': 2}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content)
+        self.assertEqual(type(result), list)
+        self.assertEqual(result[0]['company']['name'], 'Startup.Com')
+        self.assertEqual(result[0]['benefit_type']['name'], 'Vision')
+        self.assertEqual(result[0]['user']['first_name'], 'John')
