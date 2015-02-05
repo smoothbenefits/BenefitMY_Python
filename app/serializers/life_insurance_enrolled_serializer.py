5aebfbf77a76c36b1c0a1247ddb94c73f6e87b6f
Lin Lin <linlin.cqu@gmail.com>
2015-02-05 09:22:10 -0500

    life insurance enrolled seiralizer

---
+++ app/serializers/life_insurance_enrolled_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from app.models.life_insurance_enrolled import LifeInsuranceEnrolled
+from person_serializer import PersonSerializer
+
+
+class LifeInsuranceEnrolledSerializer(serializers.ModelSerializer):
+
+    person = PersonSerializer()
+
+    class Meta:
+
+        model = LifeInsuranceEnrolled
