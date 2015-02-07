f56558dad5d69ec6eb503dc2b14ac51054438802
Lin Lin <linlin.cqu@gmail.com>
2015-02-07 00:21:23 -0500

    add more direct deposit model fields

--- app/models/direct_deposit.py
+++ app/models/direct_deposit.py
@@ -11,6 +11,10 @@ class DirectDeposit(models.Model):
     routing = models.CharField(max_length=32)
     account = models.CharField(max_length=32)
     account_type = models.TextField(choices=TYPES)
-
+    bank_name = models.CharField(max_length=128, blank=True, null=True)
+    attachment = models.CharField(max_length=2048, blank=True, null=True)    # S3 link
     user = models.ForeignKey(User,
                              related_name="direct_deposit")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
