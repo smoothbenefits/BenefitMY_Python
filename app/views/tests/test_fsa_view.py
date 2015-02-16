ffc8bc610da65b542d07d9a6dd39e2e4be369d39
Lin Lin <linlin.cqu@gmail.com>
2015-02-15 21:40:27 -0500

    update fsa test

--- app/views/tests/test_fsa_view.py
+++ app/views/tests/test_fsa_view.py
@@ -14,35 +14,28 @@ class FsaTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
         result = json.loads(response.content)
-        self.assertEqual(type(result), list)
+        self.assertEqual(type(result), dict)
 
-        self.assertEqual(result[0]['amount_per_year'], '2500.00')
-        self.assertEqual(result[0]['user'], 1)
-        self.assertEqual(result[0]['person'], 2)
-        self.assertEqual(result[0]['update_reason'], 'new enroll')
-
-        self.assertEqual(result[1]['amount_per_year'], '2500.00')
-        self.assertEqual(result[1]['user'], 1)
-        self.assertEqual(result[1]['person'], 1)
-        self.assertEqual(result[1]['update_reason'], 'new enroll')
-
+        self.assertEqual(result['primary_amount_per_year'], '2500.00')
+        self.assertEqual(result['dependent_amount_per_year'], '2500.00')
+        self.assertEqual(result['user'], 1)
+        self.assertEqual(result['update_reason'], 'new enroll')
 
     def test_delete_fsa(self):
         response = self.client.get(reverse('fsa_api',
-                                           kwargs={'pk': 2}))
+                                           kwargs={'pk': 1}))
         self.assertIsNotNone(response)
         self.assertEqual(response.status_code, 200)
 
         result = json.loads(response.content)
-        self.assertEqual(type(result), list)
-
-        self.assertEqual(result[0]['amount_per_year'], '2500.00')
-        self.assertEqual(result[0]['user'], 2)
-        self.assertEqual(result[0]['person'], 3)
-        self.assertEqual(result[0]['update_reason'], 'new enroll')
+        self.assertEqual(type(result), dict)
+        self.assertEqual(result['primary_amount_per_year'], '2500.00')
+        self.assertEqual(result['dependent_amount_per_year'], '2500.00')
+        self.assertEqual(result['user'], 1)
+        self.assertEqual(result['update_reason'], 'new enroll')
 
         response = self.client.delete(reverse('fsa_api',
-                                              kwargs={'pk': 2}))
+                                              kwargs={'pk': 1}))
 
         self.assertEqual(response.status_code, 204)
         response = self.client.get(reverse('direct_deposit_api',
@@ -53,9 +46,9 @@ class FsaTestCase(TestCase):
         self.assertEqual(result['detail'], 'Not found')
 
     def test_post_fsa(self):
-        dd_data = {"amount_per_year": "2500.00",
-                   "user": 3,
-                   "person": 4,
+        dd_data = {"primary_amount_per_year": "500.00",
+                   "dependent_amount_per_year": "500.00",
+                   "user": 4,
                    "update_reason": "new enroll"}
 
         response = self.client.post(reverse('fsa_api', kwargs={'pk': 4}),
@@ -63,16 +56,15 @@ class FsaTestCase(TestCase):
         self.assertIsNotNone(response)
         self.assertEqual(response.status_code, 201)
         response = self.client.get(reverse('fsa_api',
-                                           kwargs={'pk': 3}))
+                                           kwargs={'pk': 4}))
         self.assertIsNotNone(response)
         self.assertEqual(response.status_code, 200)
         result = json.loads(response.content)
-        self.assertEqual(type(result), list)
-
-        self.assertEqual(result[0]['amount_per_year'], '2500.00')
-        self.assertEqual(result[0]['user'], 3)
-        self.assertEqual(result[0]['person'], 4)
-        self.assertEqual(result[0]['update_reason'], 'new enroll')
+        self.assertEqual(type(result), dict)
+        self.assertEqual(result['primary_amount_per_year'], '500.00')
+        self.assertEqual(result['dependent_amount_per_year'], '500.00')
+        self.assertEqual(result['user'], 4)
+        self.assertEqual(result['update_reason'], 'new enroll')
 
         #Test post duplicate data
         response = self.client.post(reverse('fsa_api', kwargs={'pk': 4}),
