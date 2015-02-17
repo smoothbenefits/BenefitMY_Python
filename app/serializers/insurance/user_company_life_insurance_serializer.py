8b4a14c5667cee773cd97c86152e45baaf7f0a61
Lin Lin <linlin.cqu@gmail.com>
2015-02-16 22:18:44 -0500

    add life insurance beneficiary serializer

--- app/serializers/insurance/user_company_life_insurance_serializer.py
+++ app/serializers/insurance/user_company_life_insurance_serializer.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 from app.models.insurance.user_company_life_insurance_plan import \
     UserCompanyLifeInsurancePlan
 
-from life_insurance_enrolled_serializer import \
-    LifeInsuranceEnrolledSerializer
+from life_insurance_beneficiary_serializer import \
+    LifeInsuranceBeneficiarySerializer
 
 
 class UserCompanyLifeInsuranceSerializer(serializers.ModelSerializer):
-    life_insurance_enrolleds = LifeInsuranceEnrolledSerializer(many=True)
+    life_insurance_enrolleds = LifeInsuranceBeneficiarySerializer(many=True)
 
     class Meta:
         model = UserCompanyLifeInsurancePlan
