f4827e1f313d3ce081f9084ee29683bcc74106c2
Lin Lin <linlin.cqu@gmail.com>
2015-02-25 22:52:28 -0500

    add life insurance beneficiary percentage

--- app/models/insurance/life_insurance_beneficiary.py
+++ app/models/insurance/life_insurance_beneficiary.py
@@ -13,5 +13,9 @@ class LifeInsuranceBeneficiary(models.Model):
                                     related_name='life_insurance_beneficiary',
                                     blank=True,
                                     null=True)
+    percentage = models.DecimalField(max_digits=5,
+                                    decimal_places=2,
+                                    null=True,
+                                    blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
