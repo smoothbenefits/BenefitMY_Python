1c014071805487e8675323e99b51d38fe0c21cab
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-03-08 11:50:47 -0400

    add contingent beneficiary to life insurance

--- app/static/js/services/services.js
+++ app/static/js/services/services.js
@@ -1053,7 +1053,22 @@ benefitmyService.factory(
           };
 
           if (memberPlanToSave.person === mainPlan.person) {
-            memberPlanToSave.life_insurance_beneficiary = mainPlan.life_insurance_beneficiary;
+
+            // insert beneficiary tier information
+            memberPlanToSave.life_insurance_beneficiary = [];
+            if (mainPlan.life_insurance_beneficiary){
+              _.each(mainPlan.life_insurance_beneficiary, function(beneficiary){
+                beneficiary.tier = "1";
+                memberPlanToSave.life_insurance_beneficiary.push(beneficiary);
+              });
+            }
+
+            if (mainPlan.life_insurance_contingent_beneficiary){
+              _.each(mainPlan.life_insurance_contingent_beneficiary, function(beneficiary){
+                beneficiary.tier = "2";
+                memberPlanToSave.life_insurance_beneficiary.push(beneficiary);
+              });
+            }
           }
 
           if (!memberPlanToSave.id) {
