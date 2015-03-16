de4fe7c00ed3c24555011b306f3228e0f55f82f2
Lin Lin <linlin.cqu@gmail.com>
2015-03-16 19:35:51 -0400

    BM277 add more info to w4

--- app/models/w4.py
+++ app/models/w4.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class W4(models.Model):
     marriage = models.IntegerField()
     dependencies = models.IntegerField()
     head = models.IntegerField()
     tax_credit = models.IntegerField()
     total_points = models.IntegerField()
+    overwrite_points = models.IntegerField(blank=True, null=True)
+    extra_amount = models.DecimalField(
+        max_digits=20, decimal_places=2, default=0, blank=True, null=True)
     user = models.ForeignKey(User,
                              related_name="w4")
