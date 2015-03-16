c8a10cd84eea5f8d52af673fa9ae79f2cb59c2a8
Lin Lin <linlin.cqu@gmail.com>
2015-03-16 19:35:51 -0400

    BM277 add more info to w4

---
+++ app/migrations/0022_auto_20150316_2332.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0021_auto_20150304_0138'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='w4',
+            name='extra_amount',
+            field=models.DecimalField(default=0, null=True, max_digits=20, decimal_places=2, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='w4',
+            name='overwrite_points',
+            field=models.IntegerField(null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
