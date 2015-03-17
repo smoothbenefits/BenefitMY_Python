93044c6beb853d8d7f14f5ddca02081bafc2d67f
Lin Lin <linlin.cqu@gmail.com>
2015-03-16 20:15:12 -0400

    BM277 w4 update

--- app/models/w4.py
+++ app/models/w4.py
@@ -8,7 +8,7 @@ class W4(models.Model):
     head = models.IntegerField()
     tax_credit = models.IntegerField()
     total_points = models.IntegerField()
-    overwrite_points = models.IntegerField(blank=True, null=True)
+    final_points = models.IntegerField(blank=True, null=True)
     extra_amount = models.DecimalField(
         max_digits=20, decimal_places=2, default=0, blank=True, null=True)
     user = models.ForeignKey(User,
