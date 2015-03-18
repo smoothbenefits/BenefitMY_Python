ed8fb7069447ab5e91184ae0f646867d7c82dc62
Lin Lin <llin@hp.com>
2015-03-18 19:24:01 -0400

    BM277 w4 unit test

---
+++ app/views/tests/test_w4_view.py
@@ -0,0 +1,79 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from view_test_base import ViewTestBase
+import json
+
+
+class w4TestCase(TestCase, ViewTestBase):
+    # your fixture files here
+    fixtures = ['23_auth_user', '35_w4']
+
+    def test_get_fsa(self):
+        response = self.client.get(reverse('w4_api',
+                                           kwargs={'pk': self.normalize_key(3)}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+
+        result = json.loads(response.content)
+        self.assertEqual(type(result), dict)
+
+        self.assertEqual(result['marriage'], 2)
+        self.assertEqual(result['tax_credit'], 0)
+        self.assertEqual(result['user'], 3)
+        self.assertEqual(result['extra_amount'], '100.00')
+
+    """"
+    def test_delete_fsa(self):
+        response = self.client.get(reverse('w4_api',
+                                           kwargs={'pk': self.normalize_key(3)}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+
+        result = json.loads(response.content)
+        self.assertEqual(type(result), dict)
+
+        self.assertEqual(result['marriage'], 2)
+        self.assertEqual(result['tax_credit'], 0)
+        self.assertEqual(result['user'], 3)
+        self.assertEqual(result['extra_amount'], '100.00')
+
+        response = self.client.delete(reverse('w4_api',
+                                              kwargs={'pk': self.normalize_key(1)}))
+
+        self.assertEqual(response.status_code, 204)
+        response = self.client.get(reverse('w4_api',
+                                           kwargs={'pk': self.normalize_key(3)}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 404)
+        result = json.loads(response.content)
+        self.assertEqual(result['detail'], 'Not found')
+    """
+
+    def test_post_w4(self):
+
+        w4_data = {
+                "marriage": 5,
+                "dependencies": 0,
+                "head": 1,
+                "tax_credit": 1,
+                "calculated_points": 3,
+                "user_defined_points": 5,
+                "extra_amount": "100.00",
+                "user": 1
+                }
+
+        response = self.client.post(reverse('w4_api', kwargs={'pk': self.normalize_key(1)}),
+                                    w4_data)
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get(reverse('w4_api',
+                                           kwargs={'pk': self.normalize_key(1)}))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content)
+        self.assertEqual(type(result), dict)
+
+        self.assertEqual(result['marriage'], 5)
+        self.assertEqual(result['tax_credit'], 1)
+        self.assertEqual(result['user'], 1)
+        self.assertEqual(result['extra_amount'], '100.00')
