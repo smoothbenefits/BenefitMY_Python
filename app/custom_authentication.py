924118e2244b09f12f1792bef7473f3ec77260fb
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-04-19 17:14:19 -0400

    Remove stealing trace.. =]

--- app/custom_authentication.py
+++ app/custom_authentication.py
@@ -7,8 +7,7 @@ from django.contrib.auth.models import (
 class AuthUserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
-        Creates and saves a User with the given email, date of
-        birth and password.
+        Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
@@ -23,8 +22,7 @@ class AuthUserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
+        Creates and saves a superuser with the given email and password.
         """
         user = self.create_user(email,
             password=password
