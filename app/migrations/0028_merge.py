cdc0e0da3d7edc69bef18d3d04928e9fe3a65ee2
Lin Lin <linlin.cqu@gmail.com>
2015-04-18 20:15:37 -0400

    add two more migration file

---
+++ app/migrations/0028_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0027_upload'),
+        ('app', '0026_auto_20150413_2325'),
+    ]
+
+    operations = [
+    ]
