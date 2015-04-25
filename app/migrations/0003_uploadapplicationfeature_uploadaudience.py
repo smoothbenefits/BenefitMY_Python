6d72dd7a772cd2f1c77a9490172088d52fca0f83
Zimeng Chen <eagleofpc@gmail.com>
2015-04-25 16:55:02 -0400

    Update migration files

--- app/migrations/0003_uploadapplicationfeature_uploadaudience.py
+++ app/migrations/0003_uploadapplicationfeature_uploadaudience.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0001_initial'),
+        ('app', '0002_auto_20150425_0221'),
     ]
 
     operations = [
