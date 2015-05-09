4d4c3e8a80877ccabfe29b5d49d17249b023b6cf
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-05-09 12:18:26 -0400

    Add condition to person company life insurance model

--- app/models/fsa/fsa.py
+++ app/models/fsa/fsa.py
@@ -17,6 +17,7 @@ class FSA(models.Model):
 
     user = models.ForeignKey(AuthUser, related_name="fsa_user")
     company_fsa_plan = models.ForeignKey(CompanyFsaPlan, related_name="fsa_company_fsa_plan", null=True)
+    condition = models.CharField(max_length=64, blank=True, null=True)
     update_reason = models.CharField(max_length=1024, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, default=datetime.now)
     updated_at = models.DateTimeField(auto_now=True, default=datetime.now)
