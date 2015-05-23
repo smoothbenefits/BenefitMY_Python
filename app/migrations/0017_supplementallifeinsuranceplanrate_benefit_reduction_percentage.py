b29b539b8bcbbdc30b931dc56875c8ce960b0338
zhangsiy <jeffzhang_misc@hotmail.com>
2015-05-22 23:30:24 -0400

    BM-449 add benefit reduction support for supplemental life

---
+++ app/migrations/0017_supplementallifeinsuranceplanrate_benefit_reduction_percentage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0016_auto_20150517_0328'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='supplementallifeinsuranceplanrate',
+            name='benefit_reduction_percentage',
+            field=models.DecimalField(null=True, max_digits=10, decimal_places=2, blank=True),
+            preserve_default=True,
+        ),
+    ]
