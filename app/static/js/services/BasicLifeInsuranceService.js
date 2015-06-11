9dde45eaf133db295635921405f176d897fe40c4
Xiaoyu Lei <xlei@vistaprint.com>
2015-06-11 18:51:23 -0400

    Update Prod patch to make sure the update on basic life insurance works as well

--- app/static/js/services/BasicLifeInsuranceService.js
+++ app/static/js/services/BasicLifeInsuranceService.js
@@ -254,7 +254,7 @@ benefitmyService.factory('BasicLifeInsuranceService',
             "id": basicLifeToSave.id,
             "user": userId,
             "person": mainPlanPerson.id,
-            "company_life_insurance": basicLifeToSave.id,
+            "company_life_insurance": basicLifeToSave.companyLifeInsurancePlan,
             "life_insurance_beneficiary": [],
             "insurance_amount": basicLifeToSave.insurance_amount
           };
