a8fa16bca9bd2a95995cb328b0304ce00accd80f
Zimeng Chen <eagleofpc@gmail.com>
2015-07-16 21:54:08 -0400

    BM-645 After onboarding, employee should be brought to family edit

--- app/static/js/controllers/EmployeeControllers.js
+++ app/static/js/controllers/EmployeeControllers.js
@@ -836,8 +836,8 @@ var onboardTax = employeeControllers.controller('onboardTax',
 }]);
 
 var onboardComplete = employeeControllers.controller('onboardComplete',
-  ['$scope', '$stateParams', '$location', 'employeeSignature', 'EmployeePreDashboardValidationService',
-  function($scope, $stateParams, $location, employeeSignature, EmployeePreDashboardValidationService){
+  ['$scope', '$stateParams', '$location', '$state', 'employeeSignature', 'EmployeePreDashboardValidationService',
+  function($scope, $stateParams, $location, $state, employeeSignature, EmployeePreDashboardValidationService){
     $scope.employee = {};
     $scope.employeeId = $stateParams.employee_id;
 
@@ -881,7 +881,7 @@ var onboardComplete = employeeControllers.controller('onboardComplete',
         };
         employeeSignature.save({userId: $scope.employeeId}, contract,
           function(){
-            $location.path('/employee');
+            $state.go('employee_family', {employeeId: $scope.employeeId, onboard:true});
           }, function(){
             alert('Failed to submit signature');
           });
@@ -2367,6 +2367,7 @@ var employeeFamilyController = employeeControllers.controller(
     $modal,
     PersonService){
 
+    $('body').removeClass('onboarding-page');
     var selfPerson = null;
     $scope.employeeId = $stateParams.employeeId;
     $scope.family=[];
@@ -2433,6 +2434,8 @@ var employeeFamilyController = employeeControllers.controller(
         }
       });
     };
+
+    $scope.isOnboarding = $stateParams.onboard === 'true';
   }
 ]);
 
