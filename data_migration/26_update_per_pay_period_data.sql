dd3b347c7300ac4f8329c5dd547867c3a1c27908
Zimeng Chen <eagleofpc@gmail.com>
2015-08-02 22:17:51 -0400

    This is the version of the migration script that should have been run during production deployment

--- data_migration/26_update_per_pay_period_data.sql
+++ data_migration/26_update_per_pay_period_data.sql
@@ -1,20 +1,20 @@
 DO $$
 BEGIN
 
---Fill the Company pay_period_definition
-
-UPDATE app_company SET pay_period_definition_id = 2;
-
-
 --First do the health numbers
 
-UPDATE app_companybenefitplanoption
-SET employee_cost_per_period = employee_cost_per_period / 0.46153846153846;
+UPDATE app_companybenefitplanoption AS cb
+SET employee_cost_per_period = cb.employee_cost_per_period / p.month_factor
+FROM app_company AS c
+JOIN app_sysperioddefinition AS p ON p.id = c.pay_period_definition_id
+WHERE c.id = cb.company_id;
 
 -- Then do the basic life
-UPDATE app_companylifeinsuranceplan 
-SET employee_cost_per_period = employee_cost_per_period / 0.46153846153846 
-WHERE employee_cost_per_period IS NOT NULL AND employee_cost_per_period > 0;
+UPDATE app_companylifeinsuranceplan AS ci
+SET employee_cost_per_period = ci.employee_cost_per_period / p.month_factor
+FROM app_company AS c
+JOIN app_sysperioddefinition AS p ON p.id = c.pay_period_definition_id
+WHERE c.id = ci.company_id AND ci.employee_cost_per_period IS NOT NULL AND ci.employee_cost_per_period > 0;
 
 END
 $$;
