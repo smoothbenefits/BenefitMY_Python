3a797c64c85f0cd5d97b66eda93182ad78c6a673
Zimeng Chen <eagleofpc@gmail.com>
2015-08-04 21:40:15 -0400

    merge with master

---
+++ app/migrations/0038_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0037_auto_20150805_0023'),
+        ('app', '0037_auto_20150804_0121'),
+    ]
+
+    operations = [
+    ]
