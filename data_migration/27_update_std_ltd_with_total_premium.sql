c67c46547be4f8c8d566ea5d4703f81d3f71bd97
Zimeng Chen <eagleofpc@gmail.com>
2015-08-11 18:06:33 -0400

    Fix the calculation issues in the migration script

--- data_migration/27_update_std_ltd_with_total_premium.sql
+++ data_migration/27_update_std_ltd_with_total_premium.sql
@@ -2,7 +2,7 @@ DO $$
 BEGIN
 
 UPDATE app_usercompanyltdinsuranceplan AS ltd
-SET total_premium_per_month = LEAST(cltd.max_benefit_monthly * 12, cltd.percentage_of_salary * ep.annual_base_salary / 100) / 12 * cltd.rate / 10
+SET total_premium_per_month = LEAST(cltd.max_benefit_monthly, cltd.percentage_of_salary * ep.annual_base_salary / 100 / 12) * cltd.rate / 10
 FROM app_usercompanyltdinsuranceplan ultd 
 join app_person AS p ON p.user_id = ultd.user_id
 JOIN app_employeeprofile AS ep ON ep.person_id = p.id
@@ -10,7 +10,7 @@ JOIN app_companyltdinsuranceplan AS cltd ON cltd.id = ultd.company_ltd_insurance
 WHERE ultd.id = ltd.id;
 
 UPDATE app_usercompanystdinsuranceplan AS std
-SET total_premium_per_month = LEAST(cstd.max_benefit_weekly * 52, cstd.percentage_of_salary * ep.annual_base_salary / 100) / 12 * cstd.rate / 10
+SET total_premium_per_month = LEAST(cstd.max_benefit_weekly, cstd.percentage_of_salary * ep.annual_base_salary / 100 / 52 ) * cstd.rate / 10
 FROM app_usercompanystdinsuranceplan ustd 
 join app_person AS p ON p.user_id = ustd.user_id
 JOIN app_employeeprofile AS ep ON ep.person_id = p.id
