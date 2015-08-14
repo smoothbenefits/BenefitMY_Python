da8987ee7b2c48f866e074bc93d7baa8a57a0382
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-08-13 20:45:47 -0400

    Add views and serializers to support time tracking for employee

--- app/serializers/employee_profile_serializer.py
+++ app/serializers/employee_profile_serializer.py
@@ -3,10 +3,12 @@ from app.models.employee_profile import EmployeeProfile
 from app.models.person import Person
 from hash_pk_serializer_base import HashPkSerializerBase
 from custom_fields.hash_field import HashField
+from sys_pay_rate_definition_serializer import SysPayRateDefinitionSerializer
 
 class EmployeeProfileSerializer(HashPkSerializerBase):
     person = HashField(source="person.id")
     company = HashField(source="company.id")
+    pay_rate = SysPayRateDefinitionSerializer()
     class Meta:
         model = EmployeeProfile
 
