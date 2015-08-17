9aae886632e22636700b8a96a1ae265eae967e8d
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-08-16 20:28:12 -0400

    Update get current compensation service

--- app/service/compensation_service.py
+++ app/service/compensation_service.py
@@ -1,9 +1,14 @@
-from app.models.employee_compensation import EmployeeCompensation
 from django.utils import timezone
+from app.models.employee_compensation import EmployeeCompensation
+from app.models.employee_profile import EmployeeProfile
 
 class CompensationService(object):
     def __init__(self, person_id):
         self.person_id = person_id
+
+    def _is_fulltime_employee(self):
+        profile = EmployeeProfile.objects.get(person_id=self.person_id)
+        return profile['employment_type'] == 'FullTime'
 
     def _get_compensation_records_order_by_effective_date(self):
         try:
@@ -16,11 +21,15 @@ class CompensationService(object):
         if not comps:
             raise ValueError('No Salary Records')
         current_salary = None
+        is_fulltime = _is_fulltime_employee()
         for comp in comps:
             if comp.effective_date < timezone.now():
-                current_salary = comp.annual_base_salary
+                if is_fulltime:
+                    current_salary = comp.annual_base_salary
+                else:
+                    # for part time employee, get projected annual wage
+                    current_salary = comp.hourly_rate * comp.projected_hour_per_month * 12
                 break
         if not current_salary:
             raise ValueError('No Salary Records')
         return current_salary
-
