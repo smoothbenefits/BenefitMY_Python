e911fb3e2928216ba9a5e26ae21042b0794be939
Xiaoyu Lei <thunder.rain.a@gmail.com>
2015-08-25 22:34:10 -0400

    Provide mask function for SSN number

--- app/serializers/person_serializer.py
+++ app/serializers/person_serializer.py
@@ -31,6 +31,7 @@ class PersonSerializer(HashPkSerializerBase):
                   'first_name',
                   'last_name',
                   'middle_name',
+                  'ssn',
                   'email',
                   'birth_date',
                   'phones',
