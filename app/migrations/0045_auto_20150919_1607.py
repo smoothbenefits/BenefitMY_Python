ff6c2552c9fc7a430af89264b6358d162a7c694b
zhangsiy <jeffzhang_misc@hotmail.com>
2015-09-19 12:08:03 -0400

    BM-799 missed migration

--- app/migrations/0045_auto_20150919_1607.py
+++ app/migrations/0045_auto_20150919_1607.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0043_auto_20150917_0138'),
+        ('app', '0044_merge'),
     ]
 
     operations = [
@@ -27,6 +27,18 @@ class Migration(migrations.Migration):
             model_name='personcompsuppllifeinsuranceplan',
             name='spouse_adad_premium_per_month',
             field=models.DecimalField(null=True, verbose_name=b'calculated adad premium for spouse', max_digits=22, decimal_places=10, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='companyltdagebasedrate',
+            name='company_ltd_insurance_plan',
+            field=models.ForeignKey(related_name='age_based_rates', to='app.CompanyLtdInsurancePlan'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='companystdagebasedrate',
+            name='company_std_insurance_plan',
+            field=models.ForeignKey(related_name='age_based_rates', to='app.CompanyStdInsurancePlan'),
             preserve_default=True,
         ),
     ]
