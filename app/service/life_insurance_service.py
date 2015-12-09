2bf2148ff75655c694b25b2d4389c892728a9583
leixiaoyu <thunder.rain.a@gmail.com>
2015-12-08 22:07:50 -0500

    Convert to float from decimal to multiple with float type

--- app/service/life_insurance_service.py
+++ app/service/life_insurance_service.py
@@ -31,7 +31,7 @@ class LifeInsuranceService(object):
 
     def _get_total_premium(self, effective_benefit_amount, rate, year_factor):
         # get total premium per month
-        total_premium = float(effective_benefit_amount * (rate / 10) * year_factor)
+        total_premium = float(effective_benefit_amount * (rate / 10)) * year_factor
         return total_premium
 
     def _get_employee_premium(self, total_premium, employee_contribution_percentage):
