73858132571cb84f1c774b91d2663ef259f4c35f
Zimeng Chen <eagleofpc@gmail.com>
2015-12-21 12:46:17 -0500

    Add back populate sql script

---
+++ data_migration/35_link_company_suppl_life_plan_to_default_group.sql
@@ -0,0 +1,26 @@
+DO $$
+BEGIN
+
+-- Find all company supplemental life plans that currently are 
+-- not linked to any company groups
+CREATE TEMP TABLE orphan_company_plans AS
+SELECT DISTINCT clip.id AS company_plan_id, clip.company_id
+FROM app_compsuppllifeinsuranceplan clip
+LEFT OUTER JOIN app_companygroupsuppllifeinsuranceplan cgblip
+ON clip.id = cgblip.company_suppl_life_insurance_plan_id
+WHERE cgblip.company_suppl_life_insurance_plan_id IS NULL;
+
+-- Get company to 'Default' company group mappings
+CREATE TEMP TABLE default_company_groups AS
+SELECT DISTINCT c.id AS company_id, cg.id AS company_default_group_id
+FROM app_company c
+LEFT OUTER JOIN app_companygroup cg ON c.id = cg.company_id AND cg.name = 'Default';
+
+INSERT INTO app_companygroupsuppllifeinsuranceplan(company_suppl_life_insurance_plan_id, company_group_id, created_at, updated_at)
+SELECT DISTINCT ocp.company_plan_id, dcg.company_default_group_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+FROM orphan_company_plans ocp
+INNER JOIN default_company_groups dcg
+ON ocp.company_id = dcg.company_id;
+
+END
+$$
