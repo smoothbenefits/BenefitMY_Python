856c5dc9ef963eeaf1784f387986d777a3c0ba72
zhangsiy <jeffzhang_misc@hotmail.com>
2016-01-13 22:55:48 -0500

    BM-952 move the email sending logic to the email service

--- app/views/company_group_member_view.py
+++ app/views/company_group_member_view.py
@@ -1,18 +1,15 @@
 from rest_framework.views import APIView
 from django.http import Http404
-from StringIO import StringIO
 from rest_framework.response import Response
 from rest_framework import status
 from app.models.company_group import CompanyGroup
 from app.models.company_group_member import CompanyGroupMember 
-from app.models.person import (Person, SELF)
+from app.models.person import Person
 from app.serializers.company_group_member_serializer import (
     CompanyGroupMemberSerializer,
     CompanyGroupMemberPostSerializer,
     CompanyGroupWithMemberSerializer)
 from app.service.send_email_service import SendEmailService
-from app.service.Report.company_employee_benefit_pdf_report_service import \
-    CompanyEmployeeBenefitPdfReportService
 
 
 class CompanyGroupMemberView(APIView):
@@ -43,52 +40,17 @@ class CompanyGroupMemberView(APIView):
             # Collect updated group information and send notfiication email
             updated_company_group_member = self._get_object(pk)
             group_member_change_info['updated_company_group'] = updated_company_group_member.company_group
-            self._send_notification_email(group_member_change_info)
+            email_service = SendEmailService()
+            email_service.send_employee_benefit_group_update_notification_email(group_member_change_info)
 
             return Response(serializer.data)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         group_member = self._get_object(pk)
         group_member.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def _send_notification_email(self, group_member_change_info):
-        send_email_service = SendEmailService()
-
-        subject = 'Employee Benefit Group Change Notification'
-        html_template_path = 'email/employee_benefit_group_change_notification.html'
-        txt_template_path = 'email/employee_benefit_group_change_notification.txt'
-
-        # build the list of target emails
-        to_emails = send_email_service.get_broker_emails_by_company(group_member_change_info['company'].id)
-
-        # get person data from user
-        group_member_change_info['person'] = group_member_change_info['user'].family.filter(relationship=SELF).first()
-        if (not group_member_change_info['person']):
-            # Use the user information if the account does not have person profile
-            # setup
-            group_member_change_info['person'] = group_member_change_info['user']
-
-        # build the template context data
-        context_data = send_email_service.get_base_email_context_data()
-        context_data['group_member_change_info'] = group_member_change_info
-
-        # get PDF
-        pdf_service = CompanyEmployeeBenefitPdfReportService()
-        pdf_buffer = StringIO()
-        pdf_service.get_employee_report(
-            group_member_change_info['user'].id,
-            group_member_change_info['company'].id,
-            pdf_buffer)
-        pdf = pdf_buffer.getvalue()
-        pdf_buffer.close()
-
-        send_email_service.send_support_email(
-            to_emails, subject, context_data, html_template_path, txt_template_path,
-            attachment_name='employee_details.pdf', attachment=pdf, attachment_mime_type='application/pdf')
-
-        return
 
 
 class CompanyGroupMemberCompanyGroupView(APIView):
