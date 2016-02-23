5faa05ef1da1872d724bf5a8318283f3d5266da4
Xiaoyu Lei <thunder.rain.a@gmail.com>
2016-02-22 21:02:39 -0500

    Add company trigger for not sign document remainder email

---
+++ app/service/notification/triggers/trigger_company_not_sign_document.py
@@ -0,0 +1,21 @@
+from datetime import date
+from trigger_not_sign_document_base import TriggerNotSignDocumentBase
+
+
+class TriggerEmployeeNotSignDocument(TriggerNotSignDocumentBase):
+    def __init__(self):
+        super(TriggerEmployeeNotSignDocument, self).__init__()
+
+    def _check_schedule(self, created_date):
+        if (not created_date):
+            return False
+
+        # Document creation should always be earlier than or the same as today
+        date_diff = (date.today() - created_date).days
+
+        # Current schedule settings:
+        #  - 35 days after created date, send notification to employer
+        if (date_diff == 35):
+            return True
+
+        return False
