0b5eaa88fd9cae44b9615ef70764fa74b12dc5ac
Xiaoyu Lei <thunder.rain.a@gmail.com>
2016-02-22 20:34:16 -0500

    Add notify company not sign document

--- app/service/notification/triggers/trigger_employee_not_sign_document.py
+++ app/service/notification/triggers/trigger_employee_not_sign_document.py
@@ -1,52 +1,10 @@
 from datetime import date
-
-from app.models.company_user import (CompanyUser, USER_TYPE_EMPLOYEE)
-from app.models.document import Document
-from trigger_base import TriggerBase
+from trigger_not_sign_document_base import TriggerNotSignDocumentBase
 
 
-class TriggerEmployeeNotSignDocument(TriggerBase):
+class TriggerEmployeeNotSignDocument(TriggerNotSignDocumentBase):
     def __init__(self):
         super(TriggerEmployeeNotSignDocument, self).__init__()
-
-    def _examine_condition(self):
-        self._refresh_cached_data()
-        print 'Inside trigger'
-        company_users = CompanyUser.objects.filter(company_user_type=USER_TYPE_EMPLOYEE)
-        print len(company_users)
-        for company_user in company_users:
-            user = company_user.user
-            company = company_user.company
-            print user.id
-            print company.id
-            user_documents = Document.objects.filter(user=user.id)
-            print len(user_documents)
-            not_signed = [doc for doc in user_documents if not doc.signature]
-            print len(not_signed)
-            if len(not_signed) > 0:
-                # sort unsigned document by creation date
-                not_signed_sorted = sorted(not_signed, key=lambda doc: doc.created_at)
-                # check schedule based on the oldest document
-                if (self._check_schedule(not_signed_sorted[0].created_at)):
-                    self._cache_company_user(company.id, user.id)
-
-        return (not self._is_cached_data_empty())
-
-    def _refresh_cached_data(self):
-        self._cached_company_user_list = dict()
-
-    def _cache_company_user(self, company_id, user_id):
-        if (company_id not in self._cached_company_user_list):
-            self._cached_company_user_list[company_id] = []
-        self._cached_company_user_list[company_id].append(user_id)
-
-    def _is_cached_data_empty(self):
-        return len(self._cached_company_user_list) <= 0
-
-    def _get_action_data(self):
-        return {
-            'company_user_id_list': self._cached_company_user_list
-        }
 
     def _check_schedule(self, created_date):
         if (not created_date):
--- app/service/tests/test_notification_service.py
+++ app/service/tests/test_notification_service.py
@@ -9,11 +9,21 @@ import re
 # Create your tests here.
 class TestNotificationService(TestCase):
 
+	fixtures = ['24_person', '49_period_definition', '10_company', '23_auth_user', '13_benefit_type',
+                '17_supplemental_life_insurance_condition', '15_benefit_policy_key',
+                '16_benefit_policy_type', 'sys_application_feature', '14_document_type',
+                '21_benefit_plan', '22_benefit_details', '26_supplemental_life_insurance',
+                '31_company_benefit_plan_option', '32_enrolled', '34_company_user',
+                '37_fsa_plan', '38_supplemental_life_rate', '39_company_supplement_life_insurance',
+                '41_user_company_benefit_plan_option', '42_company_fsa', '43_fsa',
+                '44_person_company_suppl_life', '45_suppl_life_beneficiary', '46_hra_plan',
+                '47_company_hra_plan', '48_person_company_hra_plan', 'company_features',
+                'life_insurance', 'ltd_insurance', 'std_insurance', 'waived_benefit']
 
 	''' encode a 'None' key, should return None
 	'''
 	def test_Encode_NoneKey_None(self):
 		trigger = TriggerEmployeeNotSignDocument()
 		result = trigger._examine_condition()
-		print 'After examine'
-		assert False
+		#self.assertEqual(result, True)
+		# assert False
