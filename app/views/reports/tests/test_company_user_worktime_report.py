c926fcc74cf0cf3d9e59a2c0f8abe64d871f5337
zhangsiy <jeffzhang_misc@hotmail.com>
2016-05-13 00:42:12 -0400

    BM-1121 fix unit test

--- app/views/reports/tests/test_company_user_worktime_report.py
+++ app/views/reports/tests/test_company_user_worktime_report.py
@@ -19,8 +19,8 @@ class CompanyUserWorktimeReportTests(TestCase, ViewTestBase, TimeTrackingAppMock
     def _setup_mock_url(self, start_week_start_date, end_week_start_date, company_id):
         timetracking_service_path = 'api/v1/company/{0}/work_timesheets?start_date={1}&end_date={2}'.format(
             '{0}_{1}'.format(settings.ENVIRONMENT_IDENTIFIER, self.normalize_key(company_id)),
-            start_week_start_date.strftime('%Y-%m-%dT%H:%M:%S'),
-            end_week_start_date.strftime('%Y-%m-%dT%H:%M:%S'))
+            start_week_start_date.isoformat(),
+            end_week_start_date.isoformat())
         return timetracking_service_path
 
     def _setup_mock_return_json(self, week_start_dates, company_id, user_id):
