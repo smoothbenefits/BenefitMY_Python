bec5bed6b56a0e1e111539477d7bdbc555be21f3
Zimeng Chen <eagleofpc@gmail.com>
2016-05-25 21:56:02 -0400

    Add a more generic trigger base

--- app/service/system_tasks/notification/triggers/trigger_employee_i9_expiration.py
+++ app/service/system_tasks/notification/triggers/trigger_employee_i9_expiration.py
@@ -1,13 +1,32 @@
 from datetime import date
-from trigger_i9_expiration_base import TriggerI9ExpirationBase
+from app.models.company_user import (CompanyUser, USER_TYPE_EMPLOYEE)
+from app.models.employment_authorization import EmploymentAuthorization
+from trigger_company_user_base import TriggerCompanyUserBase
+
 
 EMAIL_START_DAY = 28
-TERMINATE_NOTIFICATiON_DAY = 0
+TERMINATE_NOTIFICATION_DAY = 0
 NOTIFICATION_INTERVAL = 7
 
-class TriggerEmployeeI9Expiration(TriggerI9ExpirationBase):
+class TriggerEmployeeI9Expiration(TriggerCompanyUserBase):
     def __init__(self):
         super(TriggerEmployeeI9Expiration, self).__init__()
+
+    def _examine_condition(self):
+        super(TriggerEmployeeI9Expiration, self)._refresh_cache()
+        expiring_i9_list = EmploymentAuthorization.objects.all().exclude(expiration_date__isnull=True)
+
+        for expiring_i9 in expiring_i9_list:
+            if self._check_schedule(expiring_i9.expiration_date):
+                company_user = CompanyUser.objects.filter(
+                    user=expiring_i9.user,
+                    company_user_type=USER_TYPE_EMPLOYEE
+                ).first()
+
+                if company_user:
+                    self._cache_company_user(company_user.company.id, company_user.user.id)
+
+        return (not self._is_cached_data_empty())
 
     def _check_schedule(self, expiration_date):
         if (not expiration_date):
@@ -20,7 +39,7 @@ class TriggerEmployeeI9Expiration(TriggerI9ExpirationBase):
         #  - between 7 days before and 0 days before expiration date, send daily
         #  - prior to 28 days before expiration date, send one every 7 days
         #  - after expiration, stop notification
-        if ((date_diff <= NOTIFICATION_INTERVAL and date_diff >= TERMINATE_NOTIFICATiON_DAY)
+        if ((date_diff <= NOTIFICATION_INTERVAL and date_diff >= TERMINATE_NOTIFICATION_DAY)
             or (date_diff <= EMAIL_START_DAY and date_diff % NOTIFICATION_INTERVAL == 0)):
             return True
         return False
