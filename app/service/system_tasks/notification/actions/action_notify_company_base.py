794ed577f0245bb4f5158ef9acbee3ac3e207ee9
Zimeng Chen <eagleofpc@gmail.com>
2016-05-27 08:42:17 -0400

    BM-1128 Send email notification to admin for expiring employee i-9 (#649)
    * First draft of I9 expiration notification trigger
    
    * Working i9 triggers
    
    * remove un-necessary changes
    
    * Add a more generic trigger base
    
    * Further refactor for trigger_company_user_base
    
    * Further refactor for the actions

--- app/service/system_tasks/notification/actions/action_notify_company_base.py
+++ app/service/system_tasks/notification/actions/action_notify_company_base.py
@@ -26,6 +26,9 @@ class ActionNotifyCompanyBase(ActionBase):
 
             # build the list of target emails
             emails = send_email_service.get_employer_emails_by_company(company_id)
+            if email_data.include_broker:
+                broker_emails = send_email_service.get_broker_emails_by_company(company_id)
+                emails.extend(broker_emails)
 
             send_email_service.send_support_email(
                 emails, email_data.subject, email_data.context_data,
@@ -39,3 +42,30 @@ class ActionNotifyCompanyBase(ActionBase):
 
     def _get_email_data(self, company_id, user_id_list):
         raise NotImplementedError()
+
+    def _get_user_view_model_list(self, user_id_list):
+        send_email_service = SendEmailService()
+        model_list = []
+        for user_id in user_id_list:
+            user = User.objects.get(pk=user_id)
+            email = send_email_service.get_email_address_by_user(user_id)
+            person = None
+            persons = Person.objects.filter(user=user.id, relationship=SELF)
+            if (len(persons) > 0):
+                person = persons[0]
+            if (person):
+                model_list.append(
+                    {
+                        'first_name': person.first_name,
+                        'last_name': person.last_name,
+                        'email': email
+                    })
+            else:
+                model_list.append(
+                    {
+                        'first_name': user.first_name,
+                        'last_name': user.last_name,
+                        'email': email
+                    })
+
+        return model_list
