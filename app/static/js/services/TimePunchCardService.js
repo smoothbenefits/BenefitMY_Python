984c1674982138779ec3d318c46e7d5059646e8e
zhangsiy <jeffzhang_misc@hotmail.com>
2016-07-18 21:11:14 -0400

    BM-1188 fix a line left in testing mode

--- app/static/js/services/TimePunchCardService.js
+++ app/static/js/services/TimePunchCardService.js
@@ -29,7 +29,7 @@ benefitmyService.factory('TimePunchCardService',
             'HourlyRate': {
                 'name': 'HourlyRate',
                 'displayText': 'Hourly Rate',
-                'adminOnly': false
+                'adminOnly': true
             },
         };
 
