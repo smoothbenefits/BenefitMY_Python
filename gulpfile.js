061ffa02ab66e846df8b2128b2b07a499c21614d
Zimeng Chen <eagleofpc@gmail.com>
2016-09-14 21:14:10 -0400

    Add collect static gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@
 var gulp,
     sass,
     merge,
-    concat;
+    concat,
+    shell;
 
 //load dependencies
 gulp = require('gulp');
 sass = require('gulp-sass');
 merge = require('merge-stream');
 concat = require('gulp-concat');
+shell = require('gulp-shell')
 
 //define default task
 gulp.task('sass', function () {
@@ -27,5 +29,6 @@ gulp.task('sass', function () {
     //merge the two streams and concatenate their contents into a single file
     return merge(sassStream, cssStream)
         .pipe(concat('theapp.css'))
-        .pipe(gulp.dest('./app/static/stylesheets/'));
+        .pipe(gulp.dest('./app/static/stylesheets/'))
+        .pipe(shell(['python manage.py collectstatic --noinput']));
 });
