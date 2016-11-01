c14a890c3f12f6520443b7cd5806fb6e598ca3cb
zhangsiy <jeffzhang_misc@hotmail.com>
2016-10-31 20:29:26 -0400

    BM-1295 revert the change to default to set password during adding new employee, per business requirements change

--- app/static/js/controllers/EmployerControllers.js
+++ app/static/js/controllers/EmployerControllers.js
@@ -188,7 +188,7 @@ var employerUser = employersController.controller('employerUser',
       };
       $scope.employment_types = EmployerEmployeeManagementService.EmploymentTypes;
       $scope.addUser = {
-        send_email:false,
+        send_email:true,
         new_employee:true,
         create_docs:true,
         employment_type: _.findWhere($scope.employment_types, function(type) {
