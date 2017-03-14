7da083d2c15beacb623df7e591106c429b5b0eff
zhangsiy <jeffzhang_misc@hotmail.com>
2017-03-14 00:25:46 -0400

    BM-1376 add logic to populate CP employee DTO

--- app/service/integration/payroll/connect_payroll/connect_payroll_data_service.py
+++ app/service/integration/payroll/connect_payroll/connect_payroll_data_service.py
@@ -1,19 +1,22 @@
+import json
 from django.contrib.auth import get_user_model
 
 from app.factory.report_view_model_factory import ReportViewModelFactory
+from app.service.company_personnel_service import CompanyPersonnelService
 from app.service.web_request_service import WebRequestService
 from app.service.integration.integration_provider_data_service_base import IntegrationProviderDataServiceBase
 from app.service.integration.integration_provider_service import (
         INTEGRATION_SERVICE_TYPE_PAYROLL,
         INTEGRATION_PAYROLL_CONNECT_PAYROLL
     )
+from connect_payroll_employee_dto import ConnectPayrollEmployeeDto
 
 User = get_user_model()
 
 # For now track the remote integration server here
 # When this is officialized, keep it in settings
-CONNECT_PAYROLL_API_BASE_URL = 'https://agilepayrollapi.azurewebsites.net/api/'
-CONNECT_PAYROLL_API_EMPLOYEE_ROUTE = 'employee'
+CONNECT_PAYROLL_API_BASE_URL = 'https://agilepayrollapi.azurewebsites.net/api/employee-navigator/'
+CONNECT_PAYROLL_API_EMPLOYEE_ROUTE = 'employees'
 
 # For testing purpose, put the below constants here
 # These should be modeled in data model, and retrieved as such.
@@ -30,36 +33,131 @@ class ConnectPayrollDataService(IntegrationProviderDataServiceBase):
     def __init__(self):
         super(ConnectPayrollDataService, self).__init__()
         self.view_model_factory = ReportViewModelFactory()
+        self.web_request_service = WebRequestService()
+        self.company_personnel_service = CompanyPersonnelService()
 
     def sync_employee_data_to_remote(self, employee_user_id):
-        employee_cp_id = self._get_employee_external_id(
+        employee_data_dto = self._get_employee_data_dto(employee_user_id)
+
+        if (employee_data_dto.payrollId):
+            # Already exists in CP system, update
+            print 'Updating Employee...'
+            self._update_employee_data_to_remote(employee_data_dto)
+        else:
+            # Does not yet exist in CP system, new employee addition, create
+            print 'Creating Employee...'
+            payroll_id = self._create_employee_data_to_remote(employee_data_dto)
+            print payroll_id
+            # Sync the cp ID from the response
+            self._set_employee_external_id(
+                    employee_user_id,
+                    INTEGRATION_SERVICE_TYPE_PAYROLL,
+                    INTEGRATION_PAYROLL_CONNECT_PAYROLL,
+                    payroll_id
+                )
+
+    def _get_employee_data_dto(self, employee_user_id):
+        # First populate the CP identifiers
+        dto = ConnectPayrollEmployeeDto()
+        dto.payrollId = self._get_employee_external_id(
                 employee_user_id,
                 INTEGRATION_SERVICE_TYPE_PAYROLL,
                 INTEGRATION_PAYROLL_CONNECT_PAYROLL
             )
-        employee_data_dto = self._get_employee_data_dto(employee_user_id)
+        if (dto.payrollId == 'ALibaba-Test'):
+            dto.payrollId = None
+        dto.companyId = self._get_cp_client_code_by_employee(employee_user_id)
+        
+        # Now populate other data
+        company_id = self.company_personnel_service.get_company_id_by_employee_user_id(employee_user_id)
+        company_info = self.view_model_factory.get_company_info(company_id)
+        person_info = self.view_model_factory.get_employee_person_info(employee_user_id)
 
-        if (employee_cp_id):
-            # Already exists in CP system, update
-            print 'Updating Employee...'
-        else:
-            # Does not yet exist in CP system, new employee addition, create
-            print 'Creating Employee...'
+        # Employee basic data
+        dto.ssn = person_info.ssn
+        dto.firstName = person_info.first_name
+        dto.lastName = person_info.last_name
+        dto.dob = self._get_date_string(person_info.birth_date)
+        dto.gender = person_info.gender
+        dto.address1 = person_info.address1
+        dto.address2 = person_info.address2
+        dto.city = person_info.city
+        dto.country = person_info.country
+        dto.state = person_info.state
+        dto.zip = person_info.zipcode
+        dto.email = person_info.email
+        if (len(person_info.phones) > 0):
+            dto.phone = person_info.phones[0]['number']
 
-        # Sync the cp ID from the response
-        self._set_employee_external_id(
-                employee_user_id,
-                INTEGRATION_SERVICE_TYPE_PAYROLL,
-                INTEGRATION_PAYROLL_CONNECT_PAYROLL,
-                'ALibaba-Test'
-            )
+        # Employment data
+        employee_profile_info = self.view_model_factory.get_employee_employment_profile_data(
+                                    employee_user_id,
+                                    company_info.company_id)
 
-        return
+        if (employee_profile_info):
+            dto.jobTitle = employee_profile_info.job_title
+            dto.fullTime = employee_profile_info.is_full_time_employee()
+            dto.hireDate = self._get_date_string(employee_profile_info.hire_date)
+            dto.originalHireDate = self._get_date_string(employee_profile_info.hire_date)
+            # [TODO]: Needs specification on employee status values
+            dto.employeeStatus = 3
+            dto.terminationDate = self._get_date_string(employee_profile_info.end_date)
 
-    def _get_employee_data_dto(self, employee_user_id):
-        return
+            # Salary data
+            self.payEffectiveDate = self._get_date_string(employee_profile_info.compensation_effective_date)
+            self.annualBaseSalary = employee_profile_info.annual_salary
+            self.baseHourlyRate = employee_profile_info.current_hourly_rate
+            self.hoursPerWeek = employee_profile_info.projected_hours_per_week  
+
+        # Other
+        employee_i9_info = self.view_model_factory.get_employee_i9_data(employee_user_id)
+        if (employee_i9_info):
+            self.usCitizen = employee_i9_info.citizen_data is not None
+
+        return dto
+
+    def _update_employee_data_to_remote(self, employee_data_dto): 
+        api_url = self._get_employee_api_url()
+        data = json.dumps(employee_data_dto.__dict__)
+
+        # [TODO]: Handle non-ok results
+        response = self.web_request_service.put(
+            api_url,
+            data_object=data,
+            auth_token=CONNECT_PAYROLL_API_KEY) 
+
+    def _create_employee_data_to_remote(self, employee_data_dto):
+        api_url = self._get_employee_api_url()
+        data = json.dumps(employee_data_dto.__dict__)
+
+        # [TODO]: Handle non-ok results
+        response = self.web_request_service.post(
+            api_url,
+            data_object=data,
+            auth_token=CONNECT_PAYROLL_API_KEY)
+
+        # The body of the response is the payroll ID
+        print response.status_code
+        print api_url
+        print data
+        if (response.status_code == 200):
+            return response.text
+
+        return None
 
     def _get_cp_client_code_by_employee(self, employee_user_id):
         # [TODO]: This should query for the actual client code
         #         via company's service provider relational data
         return TEST_CP_CLIENT_CODE
+
+    def _get_employee_api_url(self):
+        return CONNECT_PAYROLL_API_BASE_URL + CONNECT_PAYROLL_API_EMPLOYEE_ROUTE
+
+    def _get_date_string(self, date):
+        if date:
+            try:
+                return date.isoformat()
+            except:
+                return None
+        else:
+            return None
--- app/service/integration/payroll/connect_payroll/connect_payroll_employee_dto.py
+++ app/service/integration/payroll/connect_payroll/connect_payroll_employee_dto.py
@@ -1,55 +1,55 @@
 class ConnectPayrollEmployeeDto(object):
     def __init__(self):
         # System Data
-        self.companyId = ''
-        self.payrollId = ''
-        self.timeClockId = ''
+        self.companyId = None
+        self.payrollId = None
+        self.timeClockId = None
 
         # Employee bio data and basic info
-        self.ssn = ''
-        self.firstName = ''
-        self.middleName = ''
-        self.lastName = ''
-        self.suffix = ''
+        self.ssn = None
+        self.firstName = None
+        self.middleName = None
+        self.lastName = None
+        self.suffix = None
         self.dob = ''
-        self.gender = ''
-        self.martialStatus = ''
-        self.address1 = ''
-        self.address2 = ''
-        self.address3 = ''
-        self.city = ''
-        self.county = ''
-        self.country = ''
-        self.state = ''
-        self.zip = ''
-        self.email = ''
-        self.phone = '' 
+        self.gender = None
+        self.martialStatus = None
+        self.address1 = None
+        self.address2 = None
+        self.address3 = None
+        self.city = None
+        self.county = None
+        self.country = None
+        self.state = None
+        self.zip = None
+        self.email = None
+        self.phone = None
 
         # Employment data
-        self.payrollGroup = ''
-        self.businessUnit = ''
-        self.office = ''
-        self.department = ''
-        self.division = ''
-        self.union = ''
-        self.jobTitle = ''
-        self.fullTime = ''
-        self.statutoryClass = ''
-        self.seasonal = ''
-        self.hireDate = ''
-        self.hiredOn = ''
-        self.originalHireDate = ''
-        self.employeeStatus = ''
-        self.terminationDate = ''
-        self.terminatedOn = ''
-        self.terminationReason = ''
+        self.payrollGroup = None
+        self.businessUnit = None
+        self.office = None
+        self.department = None
+        self.division = None
+        self.union = None
+        self.jobTitle = None
+        self.fullTime = None
+        self.statutoryClass = None
+        self.seasonal = None
+        self.hireDate = None
+        self.hiredOn = None
+        self.originalHireDate = None
+        self.employeeStatus = None
+        self.terminationDate = None
+        self.terminatedOn = None
+        self.terminationReason = None
 
         # Salary data
-        self.payEffectiveDate = ''
-        self.annualBaseSalary = ''
-        self.baseHourlyRate = ''
-        self.hoursPerWeek = ''  
+        self.payEffectiveDate = None
+        self.annualBaseSalary = None
+        self.baseHourlyRate = None
+        self.hoursPerWeek = None  
 
         # Other
-        self.usCitizen = ''
-        self.acaClassification = ''           
+        self.usCitizen = None
+        self.acaClassification = None           
