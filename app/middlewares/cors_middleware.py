f5e2d598661aee78249cea02cd92b2ada680e406
zhangsiy <jeffzhang_misc@hotmail.com>
2017-04-10 22:32:47 -0400

    BM-1406 add more CORS preflight request support

--- app/middlewares/cors_middleware.py
+++ app/middlewares/cors_middleware.py
@@ -9,5 +9,7 @@ class CorsMiddleware(object):
 
     def process_response(self, request, response):
         response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
 
         return response
