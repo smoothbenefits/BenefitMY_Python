acd509986de4715644a117a7e94bc0867a78ad1b
Xiaoyu Lei <thunder.rain.a@gmail.com>
2017-05-02 22:12:38 -0400

    Make sure employee pin number is with 4 digits

--- app/service/employee_pin_service.py
+++ app/service/employee_pin_service.py
@@ -1,4 +1,4 @@
 class EmployeePinService(object):
 
     def get_company_wide_unique_pin(self, company_id, user_id):
-        return '{0:02d}{0:04d}'.format(company_id, user_id)
+        return '{0:04d}'.format(user_id)
--- data_migration/46_employment_pin.sql
+++ data_migration/46_employment_pin.sql
@@ -1,7 +1,7 @@
 DO $$
 BEGIN
 
-UPDATE app_employeeprofile ep SET ep.pin = LPAD(ep.company_id, 2, '0') + LPAD(p.user_id, 2, '0')
+UPDATE app_employeeprofile ep SET ep.pin = LPAD(ep.company_id, 2, '0') + LPAD(p.user_id, 4, '0')
 INNER JOIN app_person p ON ep.person_id = p.id
 WHERE ep.pin IS NULL;
 
