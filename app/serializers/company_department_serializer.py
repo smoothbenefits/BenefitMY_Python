4918d09fd6c4dbb023189dbbfb8f4118ba6c72f1
Zimeng Chen <eagleofpc@gmail.com>
2017-10-02 22:24:04 -0400

    update to better code

--- app/serializers/company_department_serializer.py
+++ app/serializers/company_department_serializer.py
@@ -1,3 +1,4 @@
+from . import FieldWithIdOrObject
 from rest_framework import serializers
 from custom_fields.hash_field import HashField
 from hash_pk_serializer_base import HashPkSerializerBase
@@ -17,31 +18,6 @@ class CompanyDepartmentPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyDepartment
 
-class CompanyDepartmentFieldWithId(serializers.WritableField):
-    def to_native(self, obj):
-        if obj:
-            return obj.id
-        else:
-            return None
-
-
-    def from_native(self, data):
-        if not data:
-            return None
-        elif isinstance(data, unicode):
-            return CompanyDepartment.objects.get(id=int(data))
-        else:
-            comp_department = None
-            if "id" not in data:
-                comp_department = CompanyDepartmentSerializer(data=data)
-            else:
-                try:
-                    dept_object = CompanyDepartment.objects.get(id=data['id'])
-                    comp_department = CompanyDepartmentSerializer(dept_object, data=data)
-                except CompanyDepartment.DoesNotExist:
-                    comp_department = CompanyDepartmentSerializer(data=data)
-            if comp_department and comp_department.is_valid():
-                comp_department.save()
-                return comp_department.object
-            else:
-                raise ValueError('Bad Company Department value')
+class CompanyDepartmentFieldWithId(FieldWithIdOrObject):
+    model = CompanyDepartment
+    serializer = CompanyDepartmentSerializer
--- app/serializers/company_job_serializer.py
+++ app/serializers/company_job_serializer.py
@@ -1,3 +1,4 @@
+from . import FieldWithIdOrObject
 from rest_framework import serializers
 from custom_fields.hash_field import HashField
 from hash_pk_serializer_base import HashPkSerializerBase
@@ -17,35 +18,6 @@ class CompanyJobPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyJob
 
-class CompanyJobFieldWithId(serializers.WritableField):
-    def to_native(self, obj):
-        print "the job field to_native got object {}, with type {}".format(obj, type(obj))
-        if isinstance(obj, int):
-            return obj
-        elif obj:
-            return obj.id
-        else:
-            return None
-
-
-    def from_native(self, data):
-        print "the job field from_native got object {}, with type {}".format(data, type(data))
-        if not data:
-            return None
-        elif isinstance(data, unicode):
-            return CompanyJob.objects.get(id=int(data))
-        else:
-            comp_job = None
-            if "id" not in data:
-                comp_job = CompanyJobSerializer(data=data)
-            else:
-                try:
-                    job_object = CompanyJob.objects.get(id=data['id'])
-                    comp_job = CompanyJobSerializer(job_object, data=data)
-                except CompanyJob.DoesNotExist:
-                    comp_job = CompanyJobSerializer(data=data)
-            if comp_job and comp_job.is_valid():
-                comp_job.save()
-                return comp_job.object
-            else:
-                raise ValueError('Bad Company Job value')
+class CompanyJobFieldWithId(FieldWithIdOrObject):
+    model = CompanyJob
+    serializer = CompanyJobSerializer
